6b4837c15cc43773297bb20b74b5eb00a196e443
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2013-04-26 11:13:18 +0200

    add + restructure compiler flags fix code coverage flags

--- CMake/Modules/PackageCodeCoverage.cmake
+++ CMake/Modules/PackageCodeCoverage.cmake
@@ -34,7 +34,5 @@
 set ( LAMA_USE_CODE_COVERAGE FALSE CACHE BOOL "Enable / Disable use of Code Coverage" )
 
 if ( LAMA_USE_CODE_COVERAGE )
-    set ( COVERAGE_FLAGS "-fprofile-arcs -ftest-coverage" )
-# TODO: move to copmiler flags!
-    set ( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COVERAGE_FLAGS}" )
+    set ( COVERAGE_FLAGS "-fprofile-arcs -ftest-coverage " )
 endif ( LAMA_USE_CODE_COVERAGE )
