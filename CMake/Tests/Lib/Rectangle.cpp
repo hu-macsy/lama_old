eb5731efdef82fbdcf02a784b7e427bad3310de3
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2013-05-01 12:20:37 +0200

    Added CMake example to build libraries

---
+++ CMake/Tests/Lib/Rectangle.cpp
@@ -0,0 +1,11 @@
+
+void Rectangle::setValues ( double _x, double _y )
+{
+    x = _x;
+    y = _y,
+}
+
+double Rectangle::getArea ( void )
+{
+    return x * y;
+}
---
+++ CMake/Tests/Lib/Rectangle.hpp
@@ -0,0 +1,11 @@
+
+class Rectangle 
+{
+    double x;
+    double y;
+
+public:
+
+    void setValues ( double, double );
+    double getArea (void);
+};
