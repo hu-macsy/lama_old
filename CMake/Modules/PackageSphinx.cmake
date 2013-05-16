c4e02587a23ea34270cf9d6174a08b43b70607c6
Jan Ecker <jan.philipp.ecker@scai.fraunhofer.de>
2013-05-16 14:10:05 +0200

    Added install for doc and userdoc

--- CMake/Modules/PackageSphinx.cmake
+++ CMake/Modules/PackageSphinx.cmake
@@ -36,64 +36,48 @@ if ( SPHINX_FOUND )
     ### install ###
     set ( LAMA_DOC_DIR "${LAMA_SOURCE_DIR}/doc" )
     set ( SPHINX_BUILD_ROOT "${CMAKE_CURRENT_BINARY_DIR}/doc/user" )
+    set ( SPHINX_INSTALL_ROOT ${CMAKE_INSTALL_PREFIX})
     file ( MAKE_DIRECTORY ${DOXYGEN_BUILD_ROOT} )
     
     configure_file( "${CMAKE_SOURCE_DIR}/doc/user/conf.py.in" "${CMAKE_SOURCE_DIR}/doc/user/conf.py" )
     configure_file( "${CMAKE_SOURCE_DIR}/doc/user/convert_json.sh.in" "${CMAKE_CURRENT_BINARY_DIR}/doc/user/convert_json.sh" )
 
-   # The initial rm command gets rid of everything previously built by this
-   # custom command.
-
-   add_custom_command (
-      OUTPUT ${SPHINX_BUILD_ROOT}/html/index.html
-      COMMAND ${Sphinx-build_EXECUTABLE} -b html -d ${SPHINX_BUILD_ROOT}/doctrees user ${SPHINX_BUILD_ROOT}/html
-      DEPENDS ${CMAKE_SOURCE_DIR}/doc/user/conf.py
-      WORKING_DIRECTORY ${LAMA_DOC_DIR}
-   )
+    # The initial rm command gets rid of everything previously built by this
+    # custom command.
+    
+    # userdoc
+    add_custom_command (
+        OUTPUT ${SPHINX_BUILD_ROOT}/html/index.html
+        COMMAND ${Sphinx-build_EXECUTABLE} -b html -d ${SPHINX_BUILD_ROOT}/doctrees user ${SPHINX_BUILD_ROOT}/html
+        DEPENDS ${CMAKE_SOURCE_DIR}/doc/user/conf.py
+        WORKING_DIRECTORY ${LAMA_DOC_DIR}
+    )
+    # userdoc_json
+    add_custom_command (
+        OUTPUT ${SPHINX_BUILD_ROOT}/json/index.html
+        COMMAND ${Sphinx-build_EXECUTABLE} -b json -d ${SPHINX_BUILD_ROOT}/doctrees user ${SPHINX_BUILD_ROOT}/json
+        COMMAND chmod +x ${SPHINX_BUILD_ROOT}/convert_json.sh
+        COMMAND ${SPHINX_BUILD_ROOT}/convert_json.sh
+        DEPENDS ${CMAKE_SOURCE_DIR}/doc/user/conf.py
+        DEPENDS ${SPHINX_BUILD_ROOT}/convert_json.sh
+        WORKING_DIRECTORY ${LAMA_DOC_DIR}
+    )
    
-   add_custom_command (
-      OUTPUT ${SPHINX_BUILD_ROOT}/json/index.html
-      COMMAND ${Sphinx-build_EXECUTABLE} -b json -d ${SPHINX_BUILD_ROOT}/doctrees user ${SPHINX_BUILD_ROOT}/json
-      COMMAND chmod +x ${SPHINX_BUILD_ROOT}/convert_json.sh
-      COMMAND ${SPHINX_BUILD_ROOT}/convert_json.sh
-      DEPENDS ${CMAKE_SOURCE_DIR}/doc/user/conf.py
-      DEPENDS ${SPHINX_BUILD_ROOT}/convert_json.sh
-      WORKING_DIRECTORY ${LAMA_DOC_DIR}
-   )
-   
-   #$(SPHINXBUILD) -b json $(ALLSPHINXOPTS) $(BUILDDIR)/json
-   # cd $(BUILDDIR)/json; \
-   # echo `pwd`; \
-   # find ./ -type f -exec sed -i 's/\.\.\/\_images/fileadmin\/LAMA\/json\/_images/g' {} \; \
-   # && find ./ -type f -exec sed -i 's/<img/<image/g' {} \; \
-   # && find ./ -type f -exec sed -i 's/\.\.\/fileadmin/fileadmin/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"configuration\/\\\"/\\\"configuration\/index\/\\\"/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"installation\/\\\"/\\\"installation\/index\/\\\"/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"tutorial\/\\\"/\\\"tutorial\/index\/\\\"/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"lecture\/\\\"/\\\"lecture\/index\/\\\"/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"reference\/\\\"/\\\"reference\/index\/\\\"/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"testing\/\\\"/\\\"testing\/index\/\\\"/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"benchmarks\/\\\"/\\\"benchmarks\/index\/\\\"/g' {} \; \
-#    && find ./ -type f -exec sed -i 's/\\\"developer\/\\\"/\\\"developer\/index\/\\\"/g' {} \;\
-#    && find ./ -type f -exec sed -i 's/\\\"solver\/\\\"/\\\"solver\/index\/\\\"/g' {} \;
-#    @echo
-#    @echo "Build finished; now you can process the JSON files."
-
-   add_custom_target (
-      userdoc
-      DEPENDS
-      ${SPHINX_BUILD_ROOT}/html/index.html
-   )
-   add_custom_target (
-      userdoc_json
-      DEPENDS
-      ${SPHINX_BUILD_ROOT}/json/index.html
-   )
-
-   # Install the documentation generated at "make" time.
-
-   # install ( DIRECTORY ${DOXYGEN_BUILD_ROOT}/ DESTINATION ${DOXYGEN_BUILD_ROOT}/html )
-
+    add_custom_target (
+        userdoc
+        DEPENDS ${SPHINX_BUILD_ROOT}/html/index.html
+    )
+    add_custom_target (
+        userdoc_json
+        DEPENDS ${SPHINX_BUILD_ROOT}/json/index.html
+    )
+    add_custom_target (
+        install_userdoc
+        COMMAND mkdir -p ${SPHINX_INSTALL_ROOT}/share/doc/lama-${LAMA_VERSION}/user
+        COMMAND cp -r ${SPHINX_BUILD_ROOT}/html/* ${SPHINX_INSTALL_ROOT}/share/doc/lama-${LAMA_VERSION}/user
+        DEPENDS ${SPHINX_BUILD_ROOT}/html/index.html
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
 else ( SPHINX_FOUND )
     if ( LAMA_CMAKE_VERBOSE )
         message ( STATUS "Not building user documentation because Sphinx not found." )
