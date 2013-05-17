9bce12edd78c359277ef641b3fa4920a8f254bff
Bea Hornef <bea.hornef@scai.fraunhofer.de>
2013-05-17 14:04:19 +0200

    tiny format changes.

--- src/examples/tutorial/matrix.cpp
+++ src/examples/tutorial/matrix.cpp
@@ -59,8 +59,8 @@ int main()
 	IndexType numColumns = 4;
     IndexType numValues  = 12;
 
-    // VERY IMPORTANT for CSR format: ia is an offset array adding values of each row starting with 0
-    // ja stores the position of each value within one line
+    // VERY IMPORTANT for CSR format: ia is an offset array adding values of each row starting with 0.
+    // ja stores the position of each value within a line,
     IndexType ia[] =
     {   0, 2, 3, 5, 8, 10, 10, 12};
     IndexType ja[] =
@@ -68,10 +68,11 @@ int main()
     ValueType matrixValues[] =
     {   6.0f, 4.0f, 7.0f, -9.3f, 4.0f, 2.0f, 5.0f, 3.0f, 2.0f, 1.0f, 1.0f, 2.0f };
 
-    // vector values for our multiplication
+    // Vector values for our multiplication.
     ValueType vectorValues[] =
     {   6.0f, 4.0f, 7.0f, -9.3f};
 
+    // All data has to be stored in LAMAArrays.
     const LAMAArray<IndexType> matrixIA = LAMAArray<IndexType>( numRows + 1, ia );
     const LAMAArray<IndexType> matrixJA = LAMAArray<IndexType>( numValues, ja );
     const LAMAArray<ValueType> mValues  = LAMAArray<ValueType>( numValues, matrixValues );
@@ -87,7 +88,7 @@ int main()
     // Allocate and fill vector for the multiplication.
     DenseVector<ValueType> vector( numColumns, 0.0 );
     vector.setValues(vValues);
-    // Allocation of the result vector
+    // Allocation of the result vector.
     DenseVector<ValueType> result( numRows, 0.0);
 
     // Distribution pointer are needed to construct a CSRSparseMatrix.
