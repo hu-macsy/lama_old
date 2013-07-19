673a1202d14667e8348f7c630bac8dae620325f7
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2013-07-19 09:32:37 +0200

    Added tests for new parallel conversion routines CSR to COO

--- src/test/CSRUtilsTest.cpp
+++ src/test/CSRUtilsTest.cpp
@@ -38,8 +38,12 @@
 #include <lama/ContextAccess.hpp>
 #include <lama/LAMAArray.hpp>
 #include <lama/LAMAInterface.hpp>
+#include <lama/HostReadAccess.hpp>
+#include <lama/HostWriteAccess.hpp>
 #include <lama/ReadAccess.hpp>
 #include <lama/WriteAccess.hpp>
+
+#include <lama/openmp/OpenMPCSRUtils.hpp>
 
 #include <test/TestMacros.hpp>
 
@@ -65,69 +69,131 @@ void absMaxDiffValTest( ContextPtr loc )
 {
     try
     {
-
-       LAMA_INTERFACE_FN_T( absMaxDiffVal, loc, CSRUtils, Reductions, ValueType );
-
+        LAMA_INTERFACE_FN_T( absMaxDiffVal, loc, CSRUtils, Reductions, ValueType );
         // input arrays
-     
         //    Array1             Array2
         //
         //    1 2 3 0 0          1 2 0 0 0
         //    0 0 1 1 2          1 0 2 2 1
-
         const IndexType ia1[] = { 0, 3, 6 };
         const IndexType ja1[] = { 0, 1, 2, 2, 3, 4 };
         const IndexType ia2[] = { 0, 2, 6 };
         const IndexType ja2[] = { 0, 1, 0, 2, 3, 4 };
         const ValueType values1[] = { 1, 2, 3, 1, 1, 2 };
         const ValueType values2[] = { 1, 2, 1, 2, 2, 1 };
-
         const IndexType numRows = 2;
         // const IndexType numColumns = 5;
         const IndexType numValues1 = sizeof( ja1 ) / sizeof( IndexType );
         const IndexType numValues2 = sizeof( ja2 ) / sizeof( IndexType );
-
         LAMAArray<IndexType> csrIA1( numRows + 1, ia1 );
         LAMAArray<IndexType> csrJA1( numValues1, ja1 );
         LAMAArray<ValueType> csrValues1( numValues1, values1 );
-
         LAMAArray<IndexType> csrIA2( numRows + 1, ia2 );
         LAMAArray<IndexType> csrJA2( numValues2, ja2 );
         LAMAArray<ValueType> csrValues2( numValues2, values2 );
-
         ReadAccess<IndexType> rCSRIA1( csrIA1, loc );
         ReadAccess<IndexType> rCSRJA1( csrJA1, loc );
         ReadAccess<ValueType> rCSRValues1( csrValues1, loc );
-
         ReadAccess<IndexType> rCSRIA2( csrIA2, loc );
         ReadAccess<IndexType> rCSRJA2( csrJA2, loc );
         ReadAccess<ValueType> rCSRValues2( csrValues2, loc );
-
         LAMA_CONTEXT_ACCESS( loc );
-
         ValueType maxVal = absMaxDiffVal( numRows, false,
                                           rCSRIA1.get(), rCSRJA1.get(), rCSRValues1.get(),
                                           rCSRIA2.get(), rCSRJA2.get(), rCSRValues2.get() );
-
         BOOST_CHECK_EQUAL( 3, maxVal );
-
         // rows are sorted, so we can alsoy apply sortFlag = true
-
         maxVal = absMaxDiffVal( numRows, true,
                                 rCSRIA1.get(), rCSRJA1.get(), rCSRValues1.get(),
                                 rCSRIA2.get(), rCSRJA2.get(), rCSRValues2.get() );
-
         BOOST_CHECK_EQUAL( 3, maxVal );
     }
-    catch( Exception )
+    catch ( Exception )
     {
         std::cout <<  "WARN: CSRUtils::absMaxDiffVal not available on " << *loc << ", not tested" << std::endl;
         return;
     }
-
 }
 
-}//namespace CSRUtilsTest
+template<typename ValueType>
+void transposeTest( ContextPtr loc )
+{
+    //  input array           transpose
+    //    1.0   -   2.0       1.0  0.5   -
+    //    0.5  0.3   -         -   0.3   - 
+    //     -    -   3.0       2.0   -   3.0 
+
+    const IndexType ia1[] = { 0, 2, 4, 5 };
+    const IndexType ja1[] = { 0, 2, 0, 1, 2 };
+    const IndexType ia2[] = { 0, 2, 3, 5 };
+    const IndexType ja2[] = { 0, 1, 1, 0, 2 };
+    const ValueType values1[] = { 1.0, 2.0, 0.5, 0.3, 3.0 };
+    const ValueType values2[] = { 1.0, 0.5, 0.3, 2.0, 3.0 };
+    const IndexType numRows = 3;
+    const IndexType numColumns = 3;
+    const IndexType numValues = 5;
+    LAMAArray<IndexType> csrIA( numRows + 1, ia1 );
+    LAMAArray<IndexType> csrJA( numValues, ja1 );
+    LAMAArray<ValueType> csrValues( numValues, values1 );
+    LAMAArray<IndexType> cscIA;
+    LAMAArray<IndexType> cscJA;
+    LAMAArray<ValueType> cscValues;
+
+    try
+    {
+        LAMA_INTERFACE_FN_T( convertCSR2CSC, loc, CSRUtils, Transpose, ValueType );
+
+        ReadAccess<IndexType> rCSRIA( csrIA, loc );
+        ReadAccess<IndexType> rCSRJA( csrJA, loc );
+        ReadAccess<ValueType> rCSRValues( csrValues, loc );
+        WriteOnlyAccess<IndexType> wCSCIA( cscIA, loc, numColumns+1 );
+        WriteOnlyAccess<IndexType> wCSCJA( cscJA, loc, numValues );
+        WriteOnlyAccess<ValueType> wCSCValues( cscValues, loc, numValues );
+
+        LAMA_CONTEXT_ACCESS( loc );
+
+        convertCSR2CSC( wCSCIA.get(), wCSCJA.get(), wCSCValues.get(),
+                        rCSRIA.get(), rCSRJA.get(), rCSRValues.get(),
+                        numRows, numColumns, numValues );
+    }
+    catch ( Exception )
+    {
+        std::cout <<  "WARN: CSRUtils::csr2csc not available on " << *loc << ", not tested" << std::endl;
+        return;
+    }
+
+    {
+        HostReadAccess<IndexType> rCSCIA( cscIA );
+        HostWriteAccess<IndexType> wCSCJA( cscJA );
+        HostWriteAccess<ValueType> wCSCValues( cscValues );
+
+
+        for ( int j = 0; j <= numColumns; ++j )
+        {
+            BOOST_CHECK_EQUAL( rCSCIA[j], ia2[j] );
+        }
+
+        // For comparison of cscJA and cscValue we need to sort it 
+
+        bool diagonalFlag = false;
+
+        OpenMPCSRUtils::sortRowElements( wCSCJA.get(), wCSCValues.get(), rCSCIA.get(),
+                                         numRows, diagonalFlag );
+
+        for ( int j = 0; j < numValues; ++j )
+        {
+            BOOST_CHECK_EQUAL( wCSCJA[j], ja2[j] );
+        }
+
+
+        for ( int j = 0; j < numValues; ++j )
+        {
+            BOOST_CHECK_EQUAL( wCSCValues[j], values2[j] );
+        }
+    }
+}
+} //namespace CSRUtilsTest
+
 } //namespace lama
 
 /* ------------------------------------------------------------------------------------------ */
@@ -137,6 +203,7 @@ BOOST_AUTO_TEST_SUITE( CSRUtilsTest );
 LAMA_LOG_DEF_LOGGER( logger, "Test.CSRUtilsTest" );
 
 LAMA_AUTO_TEST_CASE_TT( absMaxDiffValTest, CSRUtilsTest );
+LAMA_AUTO_TEST_CASE_T( transposeTest, CSRUtilsTest );
 
 /* ------------------------------------------------------------------------------------------------------------------ */
 
