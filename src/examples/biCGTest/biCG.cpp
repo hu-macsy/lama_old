2062a8ef35b7bd33187a29f3845a12aff33e3c71
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2013-07-25 14:34:07 +0200

    biCGTest example is now BiCG and no GMRES ;)

--- src/examples/biCGTest/biCG.cpp
+++ src/examples/biCGTest/biCG.cpp
@@ -1,6 +1,6 @@
 #include "Config.hpp"
 
-#include <lama/solver/GMRES.hpp>
+#include <lama/solver/BiCG.hpp>
 #include <lama/solver/criteria/IterationCount.hpp>
 #include <lama/solver/logger/CommonLogger.hpp>
 #include <lama/matrix/CSRSparseMatrix.hpp>
@@ -26,41 +26,41 @@ int main( int argc, char* argv[] )
     const char* rhsFilename;
     const char* solutionFilename;
     
-    if ( argc >= 4 )
-    {
+//    if ( argc >= 4 )
+//    {
         matrixFilename = argv[1];
-        rhsFilename = argv[2];
-        solutionFilename = argv[3];
-
-        for ( int i = 4; i < argc; ++i )
-        {
-            config.setArg( argv[i] );
-        }
-    } 
-    else 
-    {
-        cout << "Usage: " << argv[0] << " <matrix.frm> <rhs.frv> <solution.frv>" << endl;
-        exit( 1 );
-    }
+//        rhsFilename = argv[2];
+//        solutionFilename = argv[3];
+//
+//        for ( int i = 4; i < argc; ++i )
+//        {
+//            config.setArg( argv[i] );
+//        }
+//    }
+//    else
+//    {
+//        cout << "Usage: " << argv[0] << " <matrix.frm> <rhs.frv> <solution.frv>" << endl;
+//        exit( 1 );
+//    }
 
     CSRSparseMatrix<ValueType> csrMatrix( matrixFilename );
-    DenseVector<ValueType> rhs( rhsFilename );
-    DenseVector<ValueType> solution( solutionFilename );
+    DenseVector<ValueType> rhs( 675, 1.0);//rhsFilename );
+    DenseVector<ValueType> solution( 675, 0.0); //solutionFilename );
 
-    // bicg
+    // BiCG
 
     LoggerPtr slogger(
-        new CommonLogger( "<GMRES>: ", LogLevel::completeInformation, LoggerWriteBehaviour::toConsoleOnly,
+        new CommonLogger( "<BiCG>: ", LogLevel::completeInformation, LoggerWriteBehaviour::toConsoleOnly,
                           std::auto_ptr<Timer>( new Timer() ) ) );
 
-    GMRES bicgSolver( "GMRESTestSolver", slogger );
+    BiCG BiCGSolver( "BiCGTestSolver", slogger );
 
-    IndexType expectedIterations = 10;
+    IndexType expectedIterations = 50;
     CriterionPtr criterion( new IterationCount( expectedIterations ) );
 
-    bicgSolver.setStoppingCriterion( criterion );
-    bicgSolver.initialize( csrMatrix );
+    BiCGSolver.setStoppingCriterion( criterion );
+    BiCGSolver.initialize( csrMatrix );
 
-    bicgSolver.solve( solution, rhs );
+    BiCGSolver.solve( solution, rhs );
 
 }
