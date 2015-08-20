d67e1cf847e535f9cdcd43132b75a718b1852652
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2015-08-20 16:54:43 +0200

    remove thread component from boost requirements

--- CMake/Modules/Package/Boost.cmake
+++ CMake/Modules/Package/Boost.cmake
@@ -48,7 +48,7 @@ endif ( WIN32 )
 
 # Finds packages with custom search options 
 
-set ( Boost_COMPONENTS thread unit_test_framework regex system )
+set ( Boost_COMPONENTS unit_test_framework regex system )
 
 # FindBoost Debug options comment
 if    ( SCAI_CMAKE_VERBOSE )
