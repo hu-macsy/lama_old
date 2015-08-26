5b6a1f08991b53525634a051ad5172b4b746e60c
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2015-08-26 17:56:41 +0200

    fix set -std=c++11 on top cmakelists

--- CMake/Modules/ConcludeFlags.cmake
+++ CMake/Modules/ConcludeFlags.cmake
@@ -1,10 +1,10 @@
 #### concluding all defined compiler flags to CMAKE_..._FLAGS ####
 
-## scai common adds OpenMP_CXX_FLAGS to SCAI_COMMON_FLAGS if found
+## scai common adds OpenMP_CXX_FLAGS and SCAI_LANG_FLAGS to SCAI_COMMON_FLAGS if found
 if    ( SCAI_COMMON_FOUND ) 
 	set ( LAMA_CXX_FLAGS "${LAMA_CXX_FLAGS} ${SCAI_COMMON_FLAGS}" )
 else  ( SCAI_COMMON_FOUND )
-	set ( LAMA_CXX_FLAGS "${LAMA_CXX_FLAGS} ${OpenMP_CXX_FLAGS}" )
+	set ( LAMA_CXX_FLAGS "${LAMA_CXX_FLAGS} ${OpenMP_CXX_FLAGS} ${SCAI_LANG_FLAGS}" )
 endif ( SCAI_COMMON_FOUND ) 
 
 ## add variables to cache with new names so they can be modified by the user via CCMAKE
--- CMake/Modules/Findscai_common.cmake
+++ CMake/Modules/Findscai_common.cmake
@@ -31,9 +31,8 @@ include ( Package/OpenMP )
 if ( SCAI_COMMON_INCLUDE_DIR )
 
 	if    ( NOT CXX_SUPPORTS_C11 )
-	    #find_package( Boost ${SCAI_FIND_PACKAGE_FLAGS} )
 	    include ( Package/Boost )
-		list ( APPEND SCAI_COMMON_INCLUDE_DIR ${Boost_INCLUDE_DIR} )
+		list ( APPEND SCAI_COMMON_INCLUDE_DIR ${BOOST_INCLUDE_DIR} )
 	endif ( NOT CXX_SUPPORTS_C11 )
 	
 	if    ( OPENMP_FOUND AND USE_OPENMP )
