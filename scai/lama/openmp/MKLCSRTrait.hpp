59f4b093d3d182002f955ce870b4d2ab69aacb44
Eric Schricker <eric.schricker@scai.fraunhofer.de>
2016-02-09 17:12:40 +0100

    Added Wrapper for MKL CSR

---
+++ scai/lama/openmp/MKLCSRTrait.hpp
@@ -0,0 +1,55 @@
+/**
+ * @file MKLCSRTrait.hpp
+ *
+ * @license
+ * Copyright (c) 2009-2015
+ * Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ * for Fraunhofer-Gesellschaft
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * @endlicense
+ *
+ * @brief Definitions for MKL sparse interface
+ * @author  Eric Stricker
+ * @date 09.02.2016
+ * @since 2.0.0
+ */
+
+#pragma once
+
+// macros
+#define MKLCSR_BLAS_NAME( name, prefix ) mkl_##prefix##name
+
+// external
+
+namespace scai {
+
+namespace lama {
+
+class COMMON_DLL_IMPORTEXPORT MKLCSRTrait
+{
+public:
+	typedef int BLASIndexType;
+	typedef char BLASTrans;
+	typedef char BLASMatrix[6];
+};
+
+} /* end namespace lama */
+
+} /* end namespace scai */
