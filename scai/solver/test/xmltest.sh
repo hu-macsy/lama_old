3be37f66651f728c64f22f37164e4f54a65a3d6a
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2016-02-09 10:45:18 +0100

    solver tests fixed xmltest.sh

--- scai/solver/test/xmltest.sh
+++ scai/solver/test/xmltest.sh
@@ -43,12 +43,12 @@ ERROR_LEVEL=test_suite
 
 # Running tests serial
 echo "Running serial tests"
-./lama_test --output_format=XML --log_level=${ERROR_LEVEL} --report_level=no 1>${dirname}/serial_tests.xml
+./SolverTest --output_format=XML --log_level=${ERROR_LEVEL} --report_level=no 1>${dirname}/serial_tests.xml
 if [ -d distributed ];
 then
     # Running parallel tests serial and with two processes
     echo "Running distributed tests serial"
-    mpirun -np 3 --output-filename ${dirname}/dist_tests.xml distributed/lama_dist_test --output_format=XML --log_level=${ERROR_LEVEL} --report_level=no
+    mpirun -np 3 --output-filename ${dirname}/dist_tests.xml distributed/SolverTestDistributed --output_format=XML --log_level=${ERROR_LEVEL} --report_level=no
 
 	#for i in 2 3 4;
 	#do
