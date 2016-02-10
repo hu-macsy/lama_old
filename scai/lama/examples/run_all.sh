46e6a0f87a010fd39a796cbf00908a828c17963b
Jan Ecker <jan.philipp.ecker@scai.fraunhofer.de>
2016-02-10 13:54:04 +0100

    Added mutliple run_all.sh scripts for most examples

---
+++ scai/lama/examples/run_all.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# clean up
+make clean
+rm -rf *.exe *.o *.frm *.mtx *.frv *.amg *.vec *.trace
+
+# build examples
+make
+
+# run examples
+
+# bench/*
+bench/conversion.exe
+bench/matadd.exe
+bench/matmul.exe
+
+# check if there are unkown examples
+count=`ls -l -la bench/*.exe | wc -l`
+if [ $count -ne 3 ]; then
+    eecho "There are unknown executables in this directory, please add all examples to the related run_all.sh script!"
+    exit 1
+fi
+
+# labelrank/*
+labelrank/labelrank.exe labelrank/affinity.mtx labelrank/labels.mtx
+
+# check if there are unkown examples
+count=`ls -l -la labelrank/*.exe | wc -l`
+if [ $count -ne 1 ]; then
+    echo "There are unknown executables in this directory, please add all examples to the related run_all.sh script!"
+    exit 1
+fi
+
+# tutorial/*
+tutorial/blas1.exe
+tutorial/matrix.exe
+tutorial/scalar.exe
+tutorial/simple.exe
+tutorial/solver.exe
+tutorial/vector.exe
+# currently disabled!!!!! (change check when enabling again!)
+#tutorial/vector_exp.exe
+
+# check if there are unkown examples
+count=`ls -l -la tutorial/*.exe | wc -l`
+if [ $count -ne 6 ]; then
+    echo "There are unknown executables in this directory, please add all examples to the related run_all.sh script!"
+    exit 1
+fi
---
+++ scai/tasking/examples/run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# clean up
+make clean
+
+# build examples
+make
+
+# run examples
+./BenchTasking.exe
+./ThreadPoolTest.exe
+./Token.exe
+
+# check if there are unkown examples
+count=`ls -l -la *.exe | wc -l`
+if [ $count -ne 3 ]; then
+    echo "There are unknown executables in this directory, please add all examples to the related run_all.sh script!"
+    exit 1
+fi
