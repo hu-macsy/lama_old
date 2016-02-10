875e70745dffbd35c0c1a109fd0a64262b9f1fee
Jan Ecker <jan.philipp.ecker@scai.fraunhofer.de>
2016-02-10 12:36:59 +0100

    Added run_all.sh to the examples of logging Small fix in run_all.sh of common

--- scai/common/examples/run_all.sh
+++ scai/common/examples/run_all.sh
@@ -25,7 +25,6 @@ make
 # check if there are unkown examples
 count=`ls -l -la *.exe | wc -l`
 if [ $count -ne 13 ]; then
-    echo $count
     echo "There are unknown executables in this directory, please add all examples to the related run_all.sh script!"
     exit 1
 fi
