1cba35a69c0e3c1d77558a987cffc06ceb6f1c8b
Jan Ecker <jan.philipp.ecker@scai.fraunhofer.de>
2016-02-17 09:56:54 +0100

    Further improvements for the run_all script of the examples of scai common

--- scai/common/examples/run_all.sh
+++ scai/common/examples/run_all.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+
+# Makes the bash exit if one commands returns with an error
 set -e
+
+echo ""
+echo "======================================================="
+echo "==  Building and executing all scai common examples  =="
+echo "======================================================="
+echo ""
 
 # clean up
 make clean
@@ -7,30 +15,30 @@ make clean
 # build examples
 make
 
-function checkErrorValue( ) {
-    $*
-    if [ "$?" -ne 0 ];
-    then
-        echo "Example: $cmd Failed. Aborting..."
-        exit 1
-    fi
-}
+# Use a counter to keep track of the number of executed examples
+i=0
 
 # run examples
+./Barrier.exe                       ;i=$((i+1))
+./CriticalRegion.exe                ;i=$((i+1))
+./DemoComplex.exe                   ;i=$((i+1))
+./DemoFactory.exe                   ;i=$((i+1))
+./DemoFactory1.exe                  ;i=$((i+1))
+./DemoFunction.exe                  ;i=$((i+1))
+./DemoMath.exe                      ;i=$((i+1))
+./DemoPointer.exe                   ;i=$((i+1))
+./DemoSettings.exe                  ;i=$((i+1))
+./DemoTypeTrait.exe                 ;i=$((i+1))
+./ExceptionDemo.exe                 ;i=$((i+1))
+./BenchPointers.exe                 ;i=$((i+1))
+./TimePrecision.exe                 ;i=$((i+1))
+./UseModule.exe ./DummyModule.so    ;i=$((i+1))  # increment only once as they both use the UseModule.exe
+./UseModule.exe ./Module.so         
 
-checkErrorValue ./Barrier.exe
-checkErrorValue ./CriticalRegion.exe
-checkErrorValue ./DemoComplex.exe
-checkErrorValue ./DemoFactory.exe
-checkErrorValue ./DemoFactory1.exe
-checkErrorValue ./DemoFunction.exe
-checkErrorValue ./DemoMath.exe
-checkErrorValue ./DemoPointer.exe
-checkErrorValue ./DemoSettings.exe
-checkErrorValue ./DemoTypeTrait.exe
-checkErrorValue ./ExceptionDemo.exe
-checkErrorValue ./BenchPointers.exe 
-checkErrorValue ./TimePrecision.exe 
-checkErrorValue ./UseModule.exe ./DummyModule.so 
-checkErrorValue ./UseModule.exe ./Module.so 
 
+# check if there are unkown examples
+count=`ls -l -la *.exe | wc -l`
+if [ $count -ne $i ]; then
+    echo "There are unknown executables in this directory, please add all examples to the related run_all.sh script!"
+    exit 1
+fi
