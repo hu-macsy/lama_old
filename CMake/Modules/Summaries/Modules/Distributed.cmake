00b40c08fefb541d2c787961e9d549d9eae46c49
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2016-04-11 18:11:38 +0200

    added version check for gpi2, nothing found for ibverbs

--- CMake/Modules/Summaries/Modules/Distributed.cmake
+++ CMake/Modules/Summaries/Modules/Distributed.cmake
@@ -5,4 +5,10 @@ endif ( MPI_ENABLED OR GPI_ENABLED )
 
 heading3 ( "Distributed" "REQUIRED_FOUND" )
     found_message ( "MPI" "MPI_FOUND" "OPTIONAL" "Version ${MPI_VERSION} at ${SCAI_MPI_INCLUDE_DIR}" )
-    found_message ( "GPI" "GPI_FOUND" "OPTIONAL" "at ${SCAI_GPI_INCLUDE_DIR}" )
+    found_message ( "GPI" "GPI_FOUND" "OPTIONAL" "with:" )
+    message ( STATUS "                                 GPI2 Version ${GPI2_VERSION} at ${GPI2_INCLUDE_DIR}" )
+    message ( STATUS "                                 IBVERBS at ${IBVERBS_INCLUDE_DIR}" )
+    # no IBVERBS_VERSION
+    #foreach    ( _LIB GPI2 IBVERBS )
+    #    message ( STATUS "                                 ${_LIB} Version ${${_LIB}_VERSION} at ${${_LIB}_INCLUDE_DIR}" )
+    #endforeach ( _LIB GPI2 IBVERBS )
