338d2226c9328c591a22359935dc6b328a99d7f6
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2016-05-10 15:26:11 +0200

    Update license headers in CMake direcotry.

--- CMake/Tests/Lib/config.hpp
+++ CMake/Tests/Lib/config.hpp
@@ -1,3 +1,31 @@
+/**
+ * @file Lib/config.hpp
+ *
+ * @license
+ * Copyright (c) 2009-2016
+ * Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ * for Fraunhofer-Gesellschaft
+ *
+ * This file is part of the Library of Accelerated Math Applications (LAMA).
+ *
+ * LAMA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * LAMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LAMA. If not, see <http://www.gnu.org/licenses/>.
+ * @endlicense
+ *
+ * @brief ToDo: Missing description in ./Lib/config.hpp
+ * @author Thomas Brandes
+ * @date 01.05.2013
+ */
 
 #ifdef DLL_LINKING
 
