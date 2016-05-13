52e0d73dccaa1792bfa68e35096f7acbd096d384
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2016-05-13 17:59:42 +0200

    fixed broken build trough typo in cmake

--- CMake/Modules/Package/Java.cmake
+++ CMake/Modules/Package/Java.cmake
@@ -28,7 +28,7 @@
 ###
 
 if    ( NOT APPLE )
-    if    ( CMAKE_VERSION VERSION_GREATER 2.8.11 
+    if    ( CMAKE_VERSION VERSION_GREATER 2.8.11 )
         # Enable Java Compilation
         # this will set at least CMAKE_Java_COMPILER CMAKE_Java_ARCHIVE 
         find_package( Java COMPONENTS Development )
