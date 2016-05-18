a457db38e2874d128a386fbe46a3713f19e0eb54
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2016-05-18 11:20:48 +0200

    Removed one unnecessary macro for TYPELIST

--- scai/common/mepr/TypeList.hpp
+++ scai/common/mepr/TypeList.hpp
@@ -68,7 +68,7 @@ struct TypeList
 /*
  * Internal used macros for TypeList creation
  *
- * TYPELIST( NR, ... ) for usage. NR is the number of types
+ * TYPELIST( N, ... ) for usage. N is the number of types
  */
 
 #define TYPELIST_1( T1 ) scai::common::mepr::TypeList<T1,common::mepr::NullType>
@@ -80,6 +80,12 @@ struct TypeList
 #define TYPELIST_7( T1, T2, T3, T4, T5, T6, T7 ) scai::common::mepr::TypeList<T1,TYPELIST_6( T2, T3, T4, T5, T6, T7 ) >
 #define TYPELIST_8( T1, T2, T3, T4, T5, T6, T7, T8 ) scai::common::mepr::TypeList<T1,TYPELIST_7( T2, T3, T4, T5, T6, T7, T8 ) >
 
-#define __TYPELIST( NR, ... ) TYPELIST_##NR( __VA_ARGS__ )
-#define _TYPELIST( NR, ... ) __TYPELIST( NR, __VA_ARGS__ )
-#define TYPELIST( NR, ... ) _TYPELIST( NR, __VA_ARGS__ )
+// Help macro is required to guarantee that argument N is no more a macro itself
+
+#define TYPELIST_N( N, ... ) TYPELIST_##N( __VA_ARGS__ )
+
+/** Macro for type lists where the first argument N specifies the number of types.  
+ *  Be careful: the total number of arguments must be N + 1
+ */
+
+#define TYPELIST( N, ... ) TYPELIST_N( N, __VA_ARGS__ )
