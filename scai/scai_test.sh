132c7d0488ad36ba7c7b0455eed16a3926e0cf3f
Eric Schricker <eric.schricker@scai.fraunhofer.de>
2016-05-31 11:21:58 +0200

    test-scripts changed -SCAI_CONTEXT to --SCAI_CONTEXT

--- scai/scai_test.sh
+++ scai/scai_test.sh
@@ -76,72 +76,72 @@ for CTX in ${CONTEXTS[*]}
 do
 	# Tasking tests
 	echo "### taskingTest on ${CTX}"
-	./tasking/test/taskingTest -SCAI_CONTEXT=${CTX}
+	./tasking/test/taskingTest --SCAI_CONTEXT=${CTX}
 
 	# HMemo tests
 	echo "### hmemoTest on ${CTX}"
-	./hmemo/test/hmemoTest -SCAI_CONTEXT=${CTX}
+	./hmemo/test/hmemoTest --SCAI_CONTEXT=${CTX}
 
 	# KRegistry tests
 	echo "### kregistryTest on ${CTX}"
-	./kregistry/test/kregistryTest -SCAI_CONTEXT=${CTX}
+	./kregistry/test/kregistryTest --SCAI_CONTEXT=${CTX}
 
 	# BLASKernel tests
 	echo "### blaskernelTest on ${CTX}"
-	./blaskernel/test/blaskernelTest -SCAI_CONTEXT=${CTX}
+	./blaskernel/test/blaskernelTest --SCAI_CONTEXT=${CTX}
 
 	# UtilsKernel tests
 	echo "### utilskernelTest on ${CTX}"
-	./utilskernel/test/utilskernelTest -SCAI_CONTEXT=${CTX}
+	./utilskernel/test/utilskernelTest --SCAI_CONTEXT=${CTX}
 
 	# SparseKernel tests
 	echo "### sparsekernelTest on ${CTX}"
-	./sparsekernel/test/sparsekernelTest -SCAI_CONTEXT=${CTX}
+	./sparsekernel/test/sparsekernelTest --SCAI_CONTEXT=${CTX}
 
 	# DMemo tests
 	export SCAI_COMMUNICATOR=NO
 	echo "### dmemoTest on ${CTX}"
-	./dmemo/test/dmemoTest -SCAI_CONTEXT=${CTX}
+	./dmemo/test/dmemoTest --SCAI_CONTEXT=${CTX}
 	if [ "${MPI_FOUND}" != "" ]
 	then
 		export SCAI_COMMUNICATOR=MPI
-		mpirun -np 1 ./dmemo/test/dmemoTest -SCAI_CONTEXT=${CTX}
-		mpirun -np 2 ./dmemo/test/dmemoTest -SCAI_CONTEXT=${CTX}
-		mpirun -np 3 ./dmemo/test/dmemoTest -SCAI_CONTEXT=${CTX}
-		mpirun -np 4 ./dmemo/test/dmemoTest -SCAI_CONTEXT=${CTX}
+		mpirun -np 1 ./dmemo/test/dmemoTest --SCAI_CONTEXT=${CTX}
+		mpirun -np 2 ./dmemo/test/dmemoTest --SCAI_CONTEXT=${CTX}
+		mpirun -np 3 ./dmemo/test/dmemoTest --SCAI_CONTEXT=${CTX}
+		mpirun -np 4 ./dmemo/test/dmemoTest --SCAI_CONTEXT=${CTX}
 	fi
 
 	# LAMA tests
 	echo "### lama_test on ${CTX}"
 	( # lamaTest
-		./lama/test/lamaTest -SCAI_CONTEXT=${CTX}
+		./lama/test/lamaTest --SCAI_CONTEXT=${CTX}
 	)
 	( # Storage Test
-		./lama/test/storage/lamaStorageTest -SCAI_CONTEXT=${CTX}
+		./lama/test/storage/lamaStorageTest --SCAI_CONTEXT=${CTX}
 	)
 # # LAMA Distributed test removed?
 #	(
 #	    export SCAI_COMMUNICATOR=NO
-#	    ./lama/test/distributed/lamaDistTest --output_format=XML --log_level=${ERROR_LEVEL} --report_level=no 1>${dirname}/lamaDistTest_${CTX}.xml -SCAI_CONTEXT=${CTX}
+#	    ./lama/test/distributed/lamaDistTest --output_format=XML --log_level=${ERROR_LEVEL} --report_level=no 1>${dirname}/lamaDistTest_${CTX}.xml --SCAI_CONTEXT=${CTX}
 #	    if [ "${MPI_FOUND}" != "" ]
 #	    then
 #	        export SCAI_COMMUNICATOR=MPI
-#	        mpirun -np 1 ./lama/test/distributed/lamaDistTest -SCAI_CONTEXT=${CTX}
-#	        mpirun -np 2 ./lama/test/distributed/lamaDistTest -SCAI_CONTEXT=${CTX}
-#	        mpirun -np 3 ./lama/test/distributed/lamaDistTest -SCAI_CONTEXT=${CTX}
-#	        mpirun -np 4 ./lama/test/distributed/lamaDistTest -SCAI_CONTEXT=${CTX}
+#	        mpirun -np 1 ./lama/test/distributed/lamaDistTest --SCAI_CONTEXT=${CTX}
+#	        mpirun -np 2 ./lama/test/distributed/lamaDistTest --SCAI_CONTEXT=${CTX}
+#	        mpirun -np 3 ./lama/test/distributed/lamaDistTest --SCAI_CONTEXT=${CTX}
+#	        mpirun -np 4 ./lama/test/distributed/lamaDistTest --SCAI_CONTEXT=${CTX}
 #	    fi
 #    )
  	( # Matrix Test   
 	    export SCAI_COMMUNICATOR=NO
-	    ./lama/test/matrix/lamaMatrixTest -SCAI_CONTEXT=${CTX}
+	    ./lama/test/matrix/lamaMatrixTest --SCAI_CONTEXT=${CTX}
 	    if [ "${MPI_FOUND}" != "" ]
 	    then
 	        export SCAI_COMMUNICATOR=MPI
-	        mpirun -np 1 ./lama/test/matrix/lamaMatrixTest -SCAI_CONTEXT=${CTX}
-	        mpirun -np 2 ./lama/test/matrix/lamaMatrixTest -SCAI_CONTEXT=${CTX}
-	        mpirun -np 3 ./lama/test/matrix/lamaMatrixTest -SCAI_CONTEXT=${CTX}
-	        mpirun -np 4 ./lama/test/matrix/lamaMatrixTest -SCAI_CONTEXT=${CTX}
+	        mpirun -np 1 ./lama/test/matrix/lamaMatrixTest --SCAI_CONTEXT=${CTX}
+	        mpirun -np 2 ./lama/test/matrix/lamaMatrixTest --SCAI_CONTEXT=${CTX}
+	        mpirun -np 3 ./lama/test/matrix/lamaMatrixTest --SCAI_CONTEXT=${CTX}
+	        mpirun -np 4 ./lama/test/matrix/lamaMatrixTest --SCAI_CONTEXT=${CTX}
 	    fi
     )
 
@@ -149,16 +149,16 @@ do
 
 	echo "### solverTest on ${CTX}"
 	export SCAI_COMMUNICATOR=NO
-	./solver/test/solverTest -SCAI_CONTEXT=${CTX}
+	./solver/test/solverTest --SCAI_CONTEXT=${CTX}
 	export SCAI_COMMUNICATOR=NO
-	./solver/test/distributed/solverDistTest -SCAI_CONTEXT=${CTX}
+	./solver/test/distributed/solverDistTest --SCAI_CONTEXT=${CTX}
 	if [ "${MPI_FOUND}" != "" ]
 	then
 		export SCAI_COMMUNICATOR=MPI
-		mpirun -np 1 ./solver/test/distributed/solverDistTest -SCAI_CONTEXT=${CTX}
-		mpirun -np 2 ./solver/test/distributed/solverDistTest -SCAI_CONTEXT=${CTX}
-		mpirun -np 3 ./solver/test/distributed/solverDistTest -SCAI_CONTEXT=${CTX}
-		mpirun -np 4 ./solver/test/distributed/solverDistTest -SCAI_CONTEXT=${CTX}
+		mpirun -np 1 ./solver/test/distributed/solverDistTest --SCAI_CONTEXT=${CTX}
+		mpirun -np 2 ./solver/test/distributed/solverDistTest --SCAI_CONTEXT=${CTX}
+		mpirun -np 3 ./solver/test/distributed/solverDistTest --SCAI_CONTEXT=${CTX}
+		mpirun -np 4 ./solver/test/distributed/solverDistTest --SCAI_CONTEXT=${CTX}
 	fi
 done
 
