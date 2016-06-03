8fc4e0a3e0741b42d6d90e4c9a2a3924729d3b97
Lauretta Schubert <lauretta.schubert@scai.fraunhofer.de>
2016-06-03 09:45:18 +0200

    impove message of GNU HostCompilerCompatibility

--- CMake/Modules/Compiler/cuda/CheckHostCompilerCompatibility.cmake
+++ CMake/Modules/Compiler/cuda/CheckHostCompilerCompatibility.cmake
@@ -35,6 +35,11 @@ if    ( CUDA_FOUND )
 					  ERROR_VARIABLE  CUDA_CHECK_COMPILE_ERROR_VAR )
 
 	if    ( CUDA_CHECK_COMPILE_RESULT_VAR )
+		if    ( CMAKE_CXX_COMPILER_ID MATCHES GNU )
+			string ( REGEX MATCH "gcc ([0-9]+\\.[0-9]) and up" VERSION_NUMBER ${CUDA_CHECK_COMPILE_ERROR_VAR} )
+			message ( STATUS "Current CUDA Installation does not work with ${VERSION_NUMBER} - you have GCC ${CXX_COMPILER_VERSION}. Disable CUDA support." )
+		endif ( CMAKE_CXX_COMPILER_ID MATCHES GNU )
+
 		if    ( CMAKE_CXX_COMPILER_ID MATCHES Intel )
 			string ( REGEX MATCH "([0-9]+\\.[0-9])" VERSION_NUMBER ${CUDA_CHECK_COMPILE_ERROR_VAR} )
 			message ( STATUS "Current CUDA Installation only works with ICC ${VERSION_NUMBER} - you have ICC ${CXX_COMPILER_VERSION}. Disable CUDA support." )
