2d56bb0939c502b639434e9f353e69e8dd8ae014
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2016-07-24 14:31:02 +0200

    Output statement is now logging statement

--- scai/lama/io/PartitionIO.cpp
+++ scai/lama/io/PartitionIO.cpp
@@ -186,7 +186,7 @@ DistributionPtr PartitionIO::readPDistribution( const string& inFileName, Commun
 {
     utilskernel::LArray<IndexType> owners;
 
-    cout << *comm << ", read distribution from " << inFileName << endl;
+    SCAI_LOG_INFO( logger, *comm << ", read partitioned distribution from " << inFileName << endl;
 
     hmemo::HArray<IndexType> myIndexes;
 
