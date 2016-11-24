f07874e945aaeb8c53e7d02e39433d754d24a53b
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2016-11-24 16:16:39 +0100

    Added MATLAB binary IO class, passes all tests.

---
+++ scai/lama/io/MATIOStream.hpp
@@ -0,0 +1,247 @@
+/**
+ * @file MATIOStream.hpp
+ *
+ * @license
+ * Copyright (c) 2009-2016
+ * Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ * for Fraunhofer-Gesellschaft
+ *
+ * This file is part of the SCAI framework LAMA.
+ *
+ * LAMA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * LAMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LAMA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms and
+ * conditions contained in a signed written agreement between you and
+ * Fraunhofer SCAI. Please contact our distributor via info[at]scapos.com.
+ * @endlicense
+ *
+ * @brief Derived class of IOStream dedicated to MATLAB format.
+ * @author Thomas Brandes
+ * @date 23.11.2016
+ */
+#pragma once
+
+// base class
+#include <scai/lama/io/IOStream.hpp>
+
+#include <scai/common/unique_ptr.hpp>
+
+namespace scai
+{
+
+namespace lama
+{
+
+/** Extension of class IOStream for MATLAB format.
+ *
+ *  These are the extensions:
+ *
+ *    - read a data element frm the file
+ *
+ */
+
+class COMMON_DLL_IMPORTEXPORT MATIOStream : public IOStream
+{
+public:
+
+    /** Constructor of a new stream for any mode and opens it */
+
+    MATIOStream( const std::string& filename, std::ios_base::openmode mode );
+
+    /** Read the header of a MAT file */
+
+    void readMATFileHeader( int& version, IOStream::Endian& indicator );
+
+    /** Write the header of a MATFile */
+
+    void writeMATFileHeader();
+
+    /** Read one complete data element from a MATLAB file in a string. 
+     *
+     *  @param[out] dataElement new allocated char array with the element data
+     *  @return     number of bytes allocated for the read element
+     *
+     *  The read element needs further parsing to read storage/array data.
+     */
+
+    uint32_t readDataElement( common::scoped_array<char>& dataElement );
+
+    /** 
+     * @param[in] buffer is the string that contains the data element 
+     * @param[out] dataType is the type of the element
+     * @param[out] nBytes is the number of bytes used for the data element
+     * @param[out] wBytes is the total number of written bytes for the element incl header
+     * @returns    pointer where the element data is storead
+     */
+    static const char* readDataElementHeader( uint32_t& dataType, uint32_t& nBytes, uint32_t& wBytes, const char buffer[] );
+
+    uint32_t writeDataElementHeader( const uint32_t dataType, const uint32_t nBytes, bool dryRun );
+
+    enum {
+        MAT_INT8       = 1,
+        MAT_UINT8      = 2,
+        MAT_INT16      = 3,
+        MAT_UINT16     = 4,
+        MAT_INT32      = 5,
+        MAT_UINT32     = 6,
+        MAT_FLOAT      = 7,
+        MAT_DOUBLE     = 9,
+        MAT_LDOUBLE    = 11,
+        MAT_INT64      = 12,
+        MAT_UINT64     = 13,
+        MAT_MATRIX     = 14,
+        MAT_COMPRESSED = 15,
+        MAT_UTF8       = 16,
+        MAT_UTF16      = 17,
+        MAT_UTF32      = 18
+    };
+
+    enum {
+        MAT_CELL_CLASS     = 1,
+        MAT_STRUCT_CLASS   = 2,
+        MAT_OBJECT_CLASS   = 3,
+        MAT_CHAR_CLASS     = 4,
+        MAT_SPARSE_CLASS   = 5,
+        MAT_DOUBLE_CLASS   = 6,
+        MAT_FLOAT_CLASS    = 7,
+        MAT_INT8_CLASS     = 8,
+        MAT_UINT8_CLASS    = 9,
+        MAT_INT16_CLASS    = 10,
+        MAT_UINT16_CLASS   = 11,
+        MAT_INT32_CLASS    = 12,
+        MAT_UINT32_CLASS   = 13,
+        MAT_INT64_CLASS    = 14,
+        MAT_UINT64_CLASS   = 15
+    };
+
+    static common::scalar::ScalarType matlabType2ScalarType( uint32_t dataType );
+
+    static uint32_t scalarType2MatlabType( common::scalar::ScalarType dataType );
+
+    static common::scalar::ScalarType class2ScalarType( uint32_t dataType );
+
+    static uint8_t scalarType2Class( common::scalar::ScalarType stype );
+
+    template<typename ValueType>
+    uint32_t writeData( const ValueType* data, uint32_t size, bool dryRun );
+
+    template<typename ValueType>
+    static uint32_t getData( ValueType* data, uint32_t size, const char* buffer );
+
+    inline uint32_t writeString( const char* name, bool dryRun );
+
+    static inline uint32_t getString( char* name, uint32_t nameSize, const char* buffer );
+
+    uint32_t writeSparseHeader(
+        const IndexType m,
+        const IndexType n,
+        const IndexType nnz,
+        const uint32_t nBytes,
+        bool isComplex,
+        bool dryRun );
+    
+    uint32_t writeDenseHeader(
+        const IndexType m,
+        const IndexType n,
+        const uint32_t nBytes,
+        common::scalar::ScalarType stype,
+        bool dryRun );
+
+    static uint32_t getMatrixInfo( uint8_t& matClass, IndexType dims[2], IndexType& nnz, bool& isComplex, const char* data );
+
+private:
+
+    uint32_t writePadding( uint32_t size, bool dryRun );
+
+}; // class MATIOStream
+
+/* --------------------------------------------------------------------------------- */
+
+template<typename ValueType>
+uint32_t MATIOStream::writeData( const ValueType* data, uint32_t size, bool dryRun )
+{
+    common::scalar::ScalarType stype = common::getScalarType<ValueType>();
+
+    uint32_t dataType  = scalarType2MatlabType( stype );
+    uint32_t nBytes    = size * sizeof( ValueType );
+
+    uint32_t wBytes    = writeDataElementHeader( dataType, nBytes, dryRun );
+
+    if ( !dryRun )
+    {
+        std::fstream::write( reinterpret_cast<const char*>( data ), nBytes );
+    }
+
+    wBytes += nBytes;
+    wBytes += writePadding( nBytes, dryRun );
+
+    SCAI_LOG_INFO( logger, "writeData<" << stype << ">, size = " << size << ", wBytes = " << wBytes << ", dryun = " << dryRun )
+
+    return wBytes;
+}
+
+/* --------------------------------------------------------------------------------- */
+
+template<typename ValueType>
+uint32_t MATIOStream::getData( ValueType* data, uint32_t size, const char* buffer  )
+{
+    uint32_t dataType;
+    uint32_t nBytes;
+    uint32_t wBytes;
+
+    SCAI_LOG_INFO( logger, "getData( " << common::TypeTraits<ValueType>::id() << ", size = " << size << " )" )
+
+    const char* dataPtr = readDataElementHeader( dataType, nBytes, wBytes, buffer );
+
+    SCAI_ASSERT_EQ_ERROR( nBytes, size * sizeof( ValueType), "size mismatch" )
+    SCAI_ASSERT_EQ_ERROR( dataType, scalarType2MatlabType( common::TypeTraits<ValueType>::stype ), "type mismatch" )
+
+    ::memcpy( data, dataPtr, nBytes );
+
+    return wBytes;
+}
+
+/* --------------------------------------------------------------------------------- */
+
+uint32_t MATIOStream::writeString( const char* name, bool dryRun )
+{
+    uint32_t size = strlen( name );
+
+    return writeData( name, size, dryRun );
+}
+
+/* --------------------------------------------------------------------------------- */
+
+uint32_t MATIOStream::getString( char* name, uint32_t nameSize, const char* buffer  )
+{
+    uint32_t dataType;
+    uint32_t nBytes;
+    uint32_t wBytes;
+
+    const char* dataPtr = readDataElementHeader( dataType, nBytes, wBytes, buffer );
+
+    SCAI_ASSERT_EQ_ERROR( dataType, MAT_INT8, "type mismatch" )
+    SCAI_ASSERT_LT_ERROR( nBytes, nameSize - 1, "too long string" )
+
+    ::memcpy( name, dataPtr, nBytes );
+
+    name[nBytes] = '\0';   // finalize string
+
+    return wBytes;
+}
+
+} /* end namespace lama */
+
+} /* end namespace scai */
---
+++ scai/lama/io/MatlabIO.hpp
@@ -0,0 +1,179 @@
+/**
+ * @file MatlabIO.hpp
+ *
+ * @license
+ * Copyright (c) 2009-2016
+ * Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ * for Fraunhofer-Gesellschaft
+ *
+ * This file is part of the SCAI framework LAMA.
+ *
+ * LAMA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * LAMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LAMA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms and
+ * conditions contained in a signed written agreement between you and
+ * Fraunhofer SCAI. Please contact our distributor via info[at]scapos.com.
+ * @endlicense
+ *
+ * @brief Structure that contains IO routines for MAT-File Format   
+ * @author Thomas Brandes
+ * @date 10.06.2016
+ */
+
+#pragma once
+
+#include <scai/lama/io/CRTPFileIO.hpp>
+
+namespace scai
+{
+
+namespace lama
+{
+
+/** This file I/O class supports the Level 5 MAT-File Format of MATLAB.
+ *
+ *  It allows to exchange data between MATLAB and LAMA applications.
+ *
+ *   /code
+ *   save example.mat matrix
+ *   load example.mat
+ *   /endcode
+ *
+ *   /code
+ *   CSRStorge<ValueType> storage;
+ *   storage.readFromFile( "example.mat" );
+ *   storage.writeToFile( "example.mat" );
+ *   /endcode
+ *
+ *  The following topics should be observed:
+ *
+ *  - LAMA can only read and write one single data element from a MATLAB file.
+ *  - The name of the data element is is ignored when reading the element and 
+ *    each written element gets the name "LAMA".
+ *  - As the data type is stored for each element in the file, the SCAI_IO_TYPE
+ *    is ignored, i.e. each array/storage is written exactly in the format it is
+ *    and there might be an implicit type conversion during the read.
+ *  - Only the SCAI_IO_TYPE=PATTERN will be handled and in this case no sparse matrix
+ *    values are written, only the row and column indexes
+ *  - The data types LongDouble and ComplexLongDouble are not really supported
+ *    by the Level 5 MAT-File format but are full supported here by using a reserved
+ *    value of the MAT-File Data Types.
+ */
+
+class MatlabIO : 
+
+    public CRTPFileIO<MatlabIO>,         // use type conversions
+    public FileIO::Register<MatlabIO>    // register at factory
+{
+
+public:
+
+    /** Implementation of pure methdod FileIO::isSupportedMode */
+
+    virtual bool isSupportedMode( const FileMode mode ) const;
+
+    /** Implementation for Printable.:writeAt */
+
+    virtual void writeAt( std::ostream& stream ) const;
+
+    // static method to create an FileIO object for this derived class
+
+    static FileIO* create();
+
+    // registration key for factory
+
+    static std::string createValue();
+
+    /** Implementation of pure methdod FileIO::readStorageInfo */
+
+    virtual void readStorageInfo( IndexType& numRows, IndexType& numColumns, IndexType& numValues, const std::string& fileName );
+
+    /** Implementation of pure methdod FileIO::readArrayInfo */
+
+    virtual void readArrayInfo( IndexType& size, const std::string& fileName );
+
+public:
+ 
+    /** Typed version of writeStorage
+     *
+     *  This method must be available for implementation of
+     *  CRTPFileIO::writeStorage
+     */
+
+    template<typename ValueType>
+    void writeStorageImpl( const MatrixStorage<ValueType>& storage, const std::string& fileName )
+    __attribute( ( noinline ) );
+
+    /** Typed version of readStorage */
+
+    template<typename ValueType>
+    void readStorageImpl( MatrixStorage<ValueType>& storage, const std::string& fileName, const IndexType firstRow, const IndexType nRows )
+    __attribute( ( noinline ) );
+
+    /** Typed version of the writeArray */
+
+    template<typename ValueType>
+    void writeArrayImpl( const hmemo::HArray<ValueType>& array, const std::string& fileName )
+    __attribute( ( noinline ) );
+
+    /** Typed version of readArray */
+
+    template<typename ValueType>
+    void readArrayImpl( hmemo::HArray<ValueType>& array, const std::string& fileName, const IndexType first, const IndexType n )
+    __attribute( ( noinline ) );
+
+    SCAI_LOG_DECL_STATIC_LOGGER( logger );  //!< logger for IO class
+
+private:
+
+    /** Help routine to read storage data, uses IO types */
+
+    template <typename ValueType>
+    void getStorage( MatrixStorage<ValueType>& storage, const char* dataElementPtr, uint32_t nBytes );
+
+    /** Help routine to read a heterogeneous array from input data 
+     *
+     *  @param[in] data is the input data with header info about size and type
+     *  @param[in] len  is the size of the input data, used for checks to avoid out-of-range 
+     *  @param[out] array will contain the read data, might be converted to ValueType
+     */
+    template<typename ValueType>
+    static uint32_t getArrayData( hmemo::HArray<ValueType>& array, const char* data, uint32_t len );
+
+    template<typename ValueType>
+    static void readMATArray( hmemo::HArray<ValueType>& array, const char* data, const uint32_t mxType, const uint32_t nbytes );
+
+    /** Help routine to read a heterogeneous array 
+     *
+     *  @tparam ArrayType is the value type of the heterogeneous array
+     *  @tparam DataType is the type of data from the file
+     */
+    template<typename ArrayType, typename DataType>
+    static void readMATArrayImpl( hmemo::HArray<ArrayType>& array, const void* data, IndexType nBytes );
+
+    /** Write a heterogeneous array as MAT format into output file. */
+
+    template<typename ValueType>
+    static uint32_t writeArrayData( class MATIOStream& outFile, const hmemo::HArray<ValueType>& array, bool dryRun );
+
+    /** Write dense array or matrix as complete element in a Matlab file. */
+
+    template<typename ValueType>
+    void writeDenseArray( class MATIOStream& outFile, const hmemo::HArray<ValueType>& array, IndexType dims[] );
+};
+
+}
+
+}
