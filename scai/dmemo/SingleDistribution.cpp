d1d60c9ef87b9dd48fa4122366ee6d4d7d4a5b9a
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2017-01-30 09:59:16 +0100

    Added new SingleDistribution class.

---
+++ scai/dmemo/SingleDistribution.cpp
@@ -0,0 +1,223 @@
+/**
+ * @file SingleDistribution.cpp
+ *
+ * @license
+ * Copyright (c) 2009-2017
+ * Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ * for Fraunhofer-Gesellschaft
+ *
+ * This file is part of the SCAI framework LAMA.
+ *
+ * LAMA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * LAMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LAMA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms and
+ * conditions contained in a signed written agreement between you and
+ * Fraunhofer SCAI. Please contact our distributor via info[at]scapos.com.
+ * @endlicense
+ *
+ * @brief Implementation of methods for single distribution class.
+ * @author Thomas Brandes
+ * @date 30.01.2017
+ */
+
+// hpp
+#include <scai/dmemo/SingleDistribution.hpp>
+
+// common
+
+#include <scai/common/macros/assert.hpp>
+
+// std
+#include <fstream>
+
+#define MASTER 0
+
+namespace scai
+{
+
+using namespace hmemo;
+
+namespace dmemo
+{
+
+SCAI_LOG_DEF_LOGGER( SingleDistribution::logger, "Distribution.SingleDistribution" )
+
+SingleDistribution::~SingleDistribution()
+{
+    SCAI_LOG_DEBUG( logger, "~SingleDistribution" )
+}
+
+SingleDistribution::SingleDistribution( const IndexType globalSize, const CommunicatorPtr communicator, const PartitionId owner ) :
+
+    Distribution( globalSize, communicator ),
+    mOwner( owner )
+ 
+{
+    SCAI_ASSERT_VALID_INDEX_ERROR( owner, communicator->getSize(), "owner is not a valid rank of any processor" )
+
+    // owner must have the same value on all processors, otherwise this would be rather strange
+
+    SCAI_LOG_INFO( logger,
+                   "SingleDistribution of " << getGlobalSize() << " elements" << ", owner " << owner )
+}
+
+bool SingleDistribution::isLocal( const IndexType ) const
+{
+    return mCommunicator->getRank() == mOwner;
+}
+
+/* ---------------------------------------------------------------------- */
+
+PartitionId SingleDistribution::findOwner( const IndexType ) const
+{
+    return mOwner;
+}
+
+/* ---------------------------------------------------------------------- */
+
+IndexType SingleDistribution::getLocalSize() const
+{
+    if ( mOwner == mCommunicator->getRank() )
+    {
+        return mGlobalSize;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+/* ---------------------------------------------------------------------- */
+
+IndexType SingleDistribution::getBlockDistributionSize() const
+{
+    return getLocalSize();
+}
+
+/* ---------------------------------------------------------------------- */
+
+IndexType SingleDistribution::getMaxLocalSize() const
+{
+    return mGlobalSize;
+}
+
+/* ---------------------------------------------------------------------- */
+
+IndexType SingleDistribution::local2global( const IndexType localIndex ) const
+{
+    return localIndex;
+}
+
+/* ---------------------------------------------------------------------- */
+
+IndexType SingleDistribution::global2local( const IndexType globalIndex ) const
+{
+    IndexType localIndex = nIndex;
+
+    if ( mOwner == mCommunicator->getRank() )
+    {
+        localIndex = globalIndex;
+    }
+
+    return localIndex;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void SingleDistribution::computeOwners( HArray<PartitionId>& owners, const HArray<IndexType>& indexes ) const
+{
+    ContextPtr ctx = Context::getHostPtr();    // currently only available @ Host
+
+    const IndexType n = indexes.size();
+
+    WriteOnlyAccess<PartitionId> wOwners( owners, ctx, n );
+
+    // ToDo: call a kernel and allow arbitrary context
+
+    for ( IndexType i = 0; i < n; i++ )
+    {
+        wOwners[i] = mOwner;
+    }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void SingleDistribution::getOwnedIndexes( hmemo::HArray<IndexType>& myGlobalIndexes ) const
+{
+    const IndexType nLocal  = getLocalSize();
+
+    SCAI_LOG_INFO( logger, getCommunicator() << ": getOwnedIndexes, have " << nLocal << " of " << mGlobalSize )
+
+    WriteOnlyAccess<IndexType> wGlobalIndexes( myGlobalIndexes, nLocal );
+
+    for ( IndexType i = 0; i < nLocal; ++i )
+    {
+        wGlobalIndexes[ i ] = i;
+    }
+}
+
+/* ---------------------------------------------------------------------- */
+
+bool SingleDistribution::isEqual( const Distribution& other ) const
+{
+    bool isSame = false;
+
+    bool proven = proveEquality( isSame, other );
+
+    if ( proven )
+    {
+        return isSame;
+    }
+
+    if ( other.getKind() == getKind() )
+    {
+        const SingleDistribution& sOther = reinterpret_cast<const SingleDistribution&>( other );
+        isSame = sOther.mOwner == mOwner;
+    }
+
+    // we know already that global size and communicator are equal
+
+    return isSame;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void SingleDistribution::writeAt( std::ostream& stream ) const
+{
+    // write identification of this object
+    stream << "SingleDistribution( comm = " << *mCommunicator << ", owner = " << mOwner << " )";
+}
+
+/* ---------------------------------------------------------------------------------*
+ *   static create methods ( required for registration in distribution factory )    *
+ * ---------------------------------------------------------------------------------*/
+
+std::string SingleDistribution::createValue()
+{
+    return getId();
+}
+
+Distribution* SingleDistribution::create( const DistributionArguments arg )
+{
+    SCAI_LOG_INFO( logger, "create" )
+
+    // by default we create one where processor 0 is owner
+
+    return new SingleDistribution( arg.globalSize, arg.communicator, 0 );
+}
+
+} /* end namespace dmemo */
+
+} /* end namespace scai */
---
+++ scai/dmemo/SingleDistribution.hpp
@@ -0,0 +1,148 @@
+/**
+ * @file SingleDistribution.hpp
+ *
+ * @license
+ * Copyright (c) 2009-2017
+ * Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ * for Fraunhofer-Gesellschaft
+ *
+ * This file is part of the SCAI framework LAMA.
+ *
+ * LAMA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * LAMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LAMA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms and
+ * conditions contained in a signed written agreement between you and
+ * Fraunhofer SCAI. Please contact our distributor via info[at]scapos.com.
+ * @endlicense
+ *
+ * @brief SingleDistribution.hpp
+ * @author thomas Brandes
+ * @date 30.01.2017
+ */
+
+#pragma once
+
+// for dll_import
+#include <scai/common/config.hpp>
+
+// base classes
+#include <scai/dmemo/Distribution.hpp>
+
+namespace scai
+{
+
+namespace dmemo
+{
+
+/** Single distribution is a distribution where all data is mapped to one single processor.
+ *
+ *  In contrary to a replicated distribution where each processor is owner of all data a single
+ *  distribution is given if only one processor has all data.
+ *
+ */
+class COMMON_DLL_IMPORTEXPORT SingleDistribution:
+
+    public Distribution,
+    private Distribution::Register<SingleDistribution>
+
+{
+public:
+
+    /** Construct a single distribution for a number of elements on to the partitions of the passed communicator.
+     *
+     *  @param[in] globalSize   number of elements to distribute
+     *  @param[in] communicator used for the partitions onto which elements are distributed.
+     *  @param[in] owner        partition id that owns all data
+     */
+    SingleDistribution( const IndexType globalSize, const CommunicatorPtr communicator, const PartitionId owner );
+
+    virtual ~SingleDistribution();
+
+    /** Implementation of pure method Distribution::isLocal */
+
+    virtual bool isLocal( const IndexType index ) const;
+
+    /** Override the default implementation Distribution::findOwner of Distribution with solution without communication */
+
+    virtual PartitionId findOwner( const IndexType globalIndex ) const;
+
+    /** Implementation of pure method Distribution::getLocalSize 
+     *
+     *  Returns global size for the owner partition, all other return 0
+     */
+    virtual IndexType getLocalSize() const;
+
+    /** Override default implementation Distribution::getMaxLocalSize() */
+
+    virtual IndexType getMaxLocalSize() const;
+
+    /** Implementation of pure method Distribution::getBlockDistributionSize 
+     *
+     *  Note: a single distribution is a special case of a general block distribution.
+     */
+    virtual IndexType getBlockDistributionSize() const;
+
+    /** Implementation of pure method Distribution::local2global */
+
+    virtual IndexType local2global( const IndexType localIndex ) const;
+
+    /** Implementation of pure method Distribution::global2local */
+
+    virtual IndexType global2local( const IndexType globalIndex ) const;
+
+    virtual bool isEqual( const Distribution& other ) const;
+
+    virtual void writeAt( std::ostream& stream ) const;
+
+    /** Override Distribution::computeOwners with more efficient version. */
+
+    virtual void computeOwners( hmemo::HArray<PartitionId>& owners, const hmemo::HArray<IndexType>& indexes ) const;
+
+    /** Override Distribution::getOwnedIndexes with more efficient version. */
+
+    virtual void getOwnedIndexes( hmemo::HArray<IndexType>& myGlobalIndexes ) const;
+
+    /** Static method required for create to use in Distribution::Register */
+
+    static Distribution* create( const DistributionArguments args );
+
+    /** Static method required for Distribution::Register */
+
+    static std::string createValue();
+
+    virtual const char* getKind() const
+    {
+        return getId();
+    }
+
+    static const char* getId()
+    {
+        return "SINGLE";
+    }
+
+protected:
+
+    SCAI_LOG_DECL_STATIC_LOGGER( logger )
+
+private:
+
+    SingleDistribution(); // disable default constructor as it has no size
+
+    PartitionId mOwner;   //!< Partition id for the owner process.
+};
+
+} /* end namespace dmemo */
+
+} /* end namespace scai */
