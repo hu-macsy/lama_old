e4eb03588c8f6af1eb76ec3d1a9ab75a60c4aacc
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2017-05-04 17:37:02 +0200

    First version of read/write png

--- scai/lama/examples/image/ImageIO.cpp
+++ scai/lama/examples/image/ImageIO.cpp
@@ -36,10 +36,13 @@
 
 #include <scai/lama/examples/image/ImageIO.hpp>
 
+#include<png.h>
+
 namespace scai
 {
 
 using namespace utilskernel;
+using namespace hmemo;
 
 namespace lama
 {
@@ -49,16 +52,93 @@ SCAI_LOG_DEF_LOGGER( ImageIO::logger, "ImageIO" )
 template<typename ValueType>
 void ImageIO::read( GridVector<ValueType>& imageData, const std::string& inputFileName )
 {
-    IndexType width = 10;
-    IndexType height = 10;
+    char header[8];    // 8 is the maximum size that can be checked
+
+    /* open file and test for it being a png */
+
+    FILE *fp = fopen( inputFileName.c_str(), "rb");
+
+    SCAI_ASSERT_ERROR( fp, inputFileName << " could not be opened for reading " )
+
+    fread( header, 1, 8, fp);
+
+    if ( png_sig_cmp( reinterpret_cast<png_byte*>( header ), 0, 8) )
+    {
+        COMMON_THROWEXCEPTION( inputFileName << " is not recognized as a PNG file" )
+    }
+
+    /* initialize stuff */
+
+    png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+    SCAI_ASSERT_ERROR( png_ptr, "png_create_read_struct failed" )
+
+    png_infop info_ptr = png_create_info_struct(png_ptr);
+
+    SCAI_ASSERT_ERROR( info_ptr, "png_create_info_struct failed" )
+
+    SCAI_ASSERT_ERROR( !setjmp( png_jmpbuf(png_ptr)), " FAIL" )
+
+    png_init_io(png_ptr, fp);
+    png_set_sig_bytes(png_ptr, 8);
+
+    png_read_info(png_ptr, info_ptr);
+
+    IndexType width = png_get_image_width(png_ptr, info_ptr);
+    IndexType height = png_get_image_height(png_ptr, info_ptr);
+
+    png_byte color_type = png_get_color_type(png_ptr, info_ptr);
+    png_byte png_bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+    int number_of_passes = png_set_interlace_handling(png_ptr);
+
+    SCAI_LOG_ERROR( logger, "color_type = " << color_type 
+                            << ", png_bit_depth = " << png_bit_depth 
+                            << ", passes = " << number_of_passes )
+
+    png_read_update_info(png_ptr, info_ptr);
+
+    /* read file */
+
+    if ( setjmp( png_jmpbuf( png_ptr ) ) )
+    {
+        COMMON_THROWEXCEPTION( "[read_png_file] Error during read_image" );
+    }
+
+    png_bytep* row_pointers = new png_bytep[ height ];
+
+    for ( IndexType y = 0; y < height; y++ )
+    {
+        row_pointers[y] = new png_byte[ png_get_rowbytes( png_ptr, info_ptr) ];
+    }
+
+    png_read_image( png_ptr, row_pointers);
+
+    fclose(fp);
 
     common::Grid3D grid( width, height, 3 );
 
-    LArray<ValueType> x( grid.size() );
+    LArray<ValueType> x;
+    
+    {
+        WriteOnlyAccess<ValueType> wX( x, grid.size() );
+
+        for ( IndexType i = 0; i < height; ++i )
+        {
+            png_bytep row_ptr = row_pointers[ i ];
+
+            for ( IndexType j = 0; j < width; ++j )
+            {
+                 wX[ 3 * width * i + 3 * j ] = row_ptr[ 4 * j ];
+                 wX[ 3 * width * i + 3 * j + 1 ] = row_ptr[ 4 * j + 1 ];
+                 wX[ 3 * width * i + 3 * j + 1 ] = row_ptr[ 4 * j + 2 ];
+            }
+        }
+    }
+
+    // ToDo: free the allocated pointer data of bitmap
 
     imageData.swap( x, grid );
-
-    SCAI_LOG_ERROR( logger, "cannot read data from file " << inputFileName )
 }
 
 template<typename ValueType>
@@ -68,7 +148,84 @@ void ImageIO::write( const GridVector<ValueType>& imageData, const std::string& 
 
     const LArray<ValueType>& x = imageData.getLocalValues();
 
-    COMMON_THROWEXCEPTION( "write image not yet supported, data = " << x )
+    const common::Grid grid = imageData.globalGrid();
+
+    const IndexType width  = grid.size( 0 );
+    const IndexType height = grid.size( 1 );
+    const IndexType ncolor = grid.size( 2 );
+
+    SCAI_ASSERT_EQ_ERROR( 4, ncolor, "4 values per pixel expected" )
+
+    // convert the array data to png data
+
+    png_bytep png_bitmap = new png_byte[ 4 * width * height ];
+
+    {
+        ReadAccess<ValueType> rX( x );
+
+        IndexType cnt = 0;
+
+        for ( IndexType y = 0; y < height; y++ )
+        {
+            for ( IndexType x = 0; x < width; ++x )
+            {
+            
+                // png_bitmap requires r, g, b, opaque value 
+
+                png_bitmap[ cnt++ ] = static_cast<png_byte>( rX[ 3 * width * y + 3  * x ] );
+                png_bitmap[ cnt++ ] = 255;
+            }    
+        }
+    }
+
+    png_structp png_ptr;
+    png_bytep* row_pointers;
+
+    // png library expects pointer for each row
+
+    row_pointers = new png_bytep[ height ];
+
+    for ( int y = 0; y < height; ++y )
+    {
+        row_pointers[y] = png_bitmap + 4 * width;
+    }
+
+    FILE* fp = fopen( outputFileName.c_str(), "wb" );
+
+    SCAI_ASSERT_ERROR( fp, "File " << outputFileName << " could not be opened for writing" )
+
+    /* initialize stuff */
+
+    png_ptr = png_create_write_struct( PNG_LIBPNG_VER_STRING, NULL, NULL, NULL );
+
+    png_infop info_ptr = png_create_info_struct( png_ptr );
+
+    SCAI_ASSERT_ERROR( png_ptr, "png_create_write_struct failed" );
+
+    SCAI_ASSERT_ERROR( !setjmp( png_jmpbuf( png_ptr ) ), "failed" )
+
+    png_init_io( png_ptr, fp );
+
+    SCAI_ASSERT_ERROR( !setjmp( png_jmpbuf( png_ptr ) ), "failed" )
+
+     /* write header */
+
+    png_byte bit_depth = 8;
+    png_byte color_type = PNG_COLOR_TYPE_RGBA;
+
+    png_set_IHDR( png_ptr, info_ptr, width, height,
+                  bit_depth, color_type, PNG_INTERLACE_NONE,
+                  PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE ); 
+
+    png_write_info( png_ptr, info_ptr );
+
+    png_write_image( png_ptr, row_pointers );
+
+    SCAI_ASSERT_ERROR( setjmp( png_jmpbuf( png_ptr ) ), "FAIL" )
+
+    png_write_end( png_ptr, NULL );
+
+    fclose( fp );
 }
 
 // instantiate methods
