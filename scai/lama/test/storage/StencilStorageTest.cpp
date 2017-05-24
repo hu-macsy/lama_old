eac03854955a409db83028abd04a110478955c95
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2017-05-24 16:19:57 +0200

    Added separate test for StencilStorage.

---
+++ scai/lama/test/storage/StencilStorageTest.cpp
@@ -0,0 +1,152 @@
+/**
+ * @file StencilStorageTest.cpp
+ *
+ * @license
+ * Copyright (c) 2009-2017
+ * Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ * for Fraunhofer-Gesellschaft
+ *
+ * This file is part of the SCAI framework LAMA.
+ *
+ * LAMA is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * LAMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with LAMA. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms and
+ * conditions contained in a signed written agreement between you and
+ * Fraunhofer SCAI. Please contact our distributor via info[at]scapos.com.
+ * @endlicense
+ *
+ * @brief Contains some test of the template class StencilStorage
+ * @author Thomas Brandes
+ * @date 24.05.2017
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
+
+#include <scai/lama/storage/StencilStorage.hpp>
+#include <scai/lama/storage/CSRStorage.hpp>
+#include <scai/common/test/TestMacros.hpp>
+#include <scai/utilskernel/LArray.hpp>
+
+#include <scai/lama/test/storage/StorageTemplateTests.hpp>
+
+using namespace scai;
+using namespace lama;
+using namespace utilskernel;
+using namespace hmemo;
+
+/* ------------------------------------------------------------------------- */
+
+BOOST_AUTO_TEST_SUITE( StencilStorageTest )
+
+SCAI_LOG_DEF_LOGGER( logger, "Test.StencilStorageTest" )
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( constructorTest, ValueType, scai_numeric_test_types )
+{
+    const IndexType N1 = 5;
+    const IndexType N2 = 8;
+    common::Grid2D grid( N1, N2 );
+
+    common::Stencil2D<ValueType> stencil( 5 ); // take 5-point stencil
+
+    // Stencil matrix is always square
+
+    StencilStorage<ValueType> storage( grid, stencil );
+
+    BOOST_CHECK_EQUAL( N1 * N2, storage.getNumRows() );
+    BOOST_CHECK_EQUAL( N1 * N2, storage.getNumColumns() );
+}
+
+/* ------------------------------------------------------------------------------------------------------------------ */
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( convertTest, ValueType, scai_numeric_test_types )
+{
+    const IndexType N1 = 5;
+    const IndexType N2 = 8;
+    common::Grid2D grid( N1, N2 );
+
+    common::Stencil2D<ValueType> stencil( 5 ); // take 5-point stencil
+
+    StencilStorage<ValueType> stencilStorage( grid, stencil );
+    CSRStorage<ValueType> csrStorage( stencilStorage );
+
+    // 5 entries for each grid element, but subract one for all border elements
+
+    IndexType expectedNNZ = 5 * N1 * N2 - 2 * N1 - 2 * N2;
+ 
+    BOOST_CHECK_EQUAL( expectedNNZ, csrStorage.getNumValues() );
+
+    for ( IndexType i1 = 0; i1 < N1; ++i1 )
+    {
+        for ( IndexType i2 = 0; i2 < N2; ++i2 )
+        {
+            IndexType pos = grid.linearPos( i1, i2 );
+
+            // check diagonal element
+
+            BOOST_CHECK_EQUAL( ValueType( 4 ), csrStorage.getValue( pos, pos ) );
+
+            if ( i1 > 0 ) 
+            {
+                IndexType posUp = grid.linearPos( i1 - 1, i2 );
+                BOOST_CHECK_EQUAL( ValueType( -1 ), csrStorage.getValue( pos, posUp ) );
+            }
+
+            if ( i1 < N1 - 1 ) 
+            {
+                IndexType posDown = grid.linearPos( i1 + 1, i2 );
+                BOOST_CHECK_EQUAL( ValueType( -1 ), csrStorage.getValue( pos, posDown ) );
+            }
+        }
+    }
+}
+
+/* ------------------------------------------------------------------------- */
+
+BOOST_AUTO_TEST_CASE_TEMPLATE( transposeTest, ValueType, scai_numeric_test_types )
+{
+    const IndexType N1 = 7;
+    const IndexType N2 = 4;
+
+    common::Grid2D grid( N1, N2 );
+
+    const int stencilData[9] = {  -1, -2, 1,
+                                  -1, 4, 5,
+                                   3, 2, 1   };
+
+    common::Stencil2D<ValueType> stencil( 3, 3, stencilData );
+    common::Stencil2D<ValueType> stencilT;
+    stencilT.transpose( stencil );
+
+    StencilStorage<ValueType> stencilStorage( grid, stencil );
+    StencilStorage<ValueType> stencilStorageT( grid, stencilT );
+
+    CSRStorage<ValueType> csrStorage( stencilStorage );
+    csrStorage.writeToFile( "csr1.mtx" );
+    CSRStorage<ValueType> csrStorageT( stencilStorageT );
+    csrStorageT.writeToFile( "csr2.mtx" );
+
+    CSRStorage<ValueType> csrStorageT1;
+    csrStorageT1.assignTranspose( csrStorage );
+    csrStorageT.writeToFile( "csr3.mtx" );
+
+    BOOST_CHECK_EQUAL( ValueType( 0 ), csrStorageT.maxDiffNorm( csrStorageT1 ) );
+}
+
+/* ------------------------------------------------------------------------- */
+
+BOOST_AUTO_TEST_SUITE_END();
