186fe4c44e1e9c07be4058cc33d7fd78e16949a8
Thomas Brandes <Thomas.Brandes@scai.fraunhofer.de>
2017-07-02 19:34:37 +0200

    Update of cmake configuration for examples.

---
+++ CMake/Modules/Functions/buildExamples.cmake
@@ -0,0 +1,89 @@
+###
+ # @file prepareExamplesMakeInc.cmake
+ #
+ # @license
+ # Copyright (c) 2009-2016
+ # Fraunhofer Institute for Algorithms and Scientific Computing SCAI
+ # for Fraunhofer-Gesellschaft
+ #
+ # This file is part of the SCAI framework LAMA.
+ #
+ # LAMA is free software: you can redistribute it and/or modify it under the
+ # terms of the GNU Affero General Public License as published by the Free
+ # Software Foundation, either version 3 of the License, or (at your option)
+ # any later version.
+ #
+ # LAMA is distributed in the hope that it will be useful, but WITHOUT ANY
+ # WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ # FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for
+ # more details.
+ #
+ # You should have received a copy of the GNU Affero General Public License
+ # along with LAMA. If not, see <http://www.gnu.org/licenses/>.
+ #
+ # Other Usage
+ # Alternatively, this file may be used in accordance with the terms and
+ # conditions contained in a signed written agreement between you and
+ # Fraunhofer SCAI. Please contact our distributor via info[at]scapos.com.
+ # @endlicense
+ #
+ # @brief Macro setting the configuration for make.inc in examples: right link libraries and -D flags
+ # @author Lauretta Schubert
+ # @date 02.03.2016
+###
+
+macro ( genExampleMakefile )
+
+    ## set SCAI_EXAMPLE_LINK_LIBRARIES with own project and all dependent libraries
+
+    set ( SCAI_EXAMPLE_LINK_LIBRARIES "-l${PROJECT_NAME}" )
+
+    set ( REVERT_LIST ${INTERNAL_DEPS} ) # because list does not accept variable recursion
+
+    if ( REVERT_LIST ) # is empty for common
+        list ( REVERSE REVERT_LIST )
+        foreach    ( module ${REVERT_LIST} )
+            set ( library "${SCAI_LIBRARY_PREFIX}${module}" )
+            set ( SCAI_EXAMPLE_LINK_LIBRARIES "${SCAI_EXAMPLE_LINK_LIBRARIES} -l${library}" )
+        endforeach ( module ${REVERT_LIST} )
+    endif ( REVERT_LIST )
+
+    ## set project specific SCAI_DEFINES
+
+    if    ( SCAI_ASSERT_LEVEL )
+        set ( SCAI_DEFINES "${SCAI_DEFINES} -DSCAI_ASSERT_LEVEL_${SCAI_ASSERT_LEVEL}" )
+    endif ( SCAI_ASSERT_LEVEL )
+    
+    if    ( SCAI_LOGGING_LEVEL )
+        set ( SCAI_DEFINES "${SCAI_DEFINES} -DSCAI_LOG_LEVEL_${SCAI_LOGGING_LEVEL}" )
+    endif ( SCAI_LOGGING_LEVEL )
+
+    if    ( SCAI_TRACING )
+        set ( SCAI_DEFINES "${SCAI_DEFINES} -DSCAI_TRACING_${SCAI_TRACING}" )
+    endif ( SCAI_TRACING_FLAG )
+
+    configure_file ( "${CMAKE_SOURCE_DIR}/examples_make.inc.in" "${CMAKE_CURRENT_BINARY_DIR}/make.inc" )
+    configure_file ( "${CMAKE_SOURCE_DIR}/examples_Makefile.in" "${CMAKE_CURRENT_BINARY_DIR}/install_Makefile" COPYONLY )
+
+    install ( FILES       ${CMAKE_CURRENT_BINARY_DIR}/make.inc 
+              DESTINATION ${INSTALL_EXAMPLE_DIR} )
+
+    install ( FILES       ${CMAKE_CURRENT_BINARY_DIR}/install_Makefile
+              DESTINATION ${INSTALL_EXAMPLE_DIR}
+              RENAME      Makefile  )
+
+endmacro ()
+
+macro ( genRunScript )
+
+    ## run script might be called in build/binary directory
+
+    file( COPY run_all.sh DESTINATION ${CMAKE_CURRENT_BINARY_DIR} )
+
+    ## but also later in installation directory
+
+    install ( PROGRAMS    run_all.sh
+              DESTINATION ${INSTALL_EXAMPLE_DIR} )
+
+endmacro ()
+
