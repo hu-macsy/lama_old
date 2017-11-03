a55422ca82785ad0e65bbbd731988736684243b6
Andreas Longva <andreas.borgen.longva@scai.fraunhofer.de>
2017-11-03 10:37:41 +0100

    Support script-based tests in run_tests.py

--- scai/run_tests.py
+++ scai/run_tests.py
@@ -8,28 +8,32 @@ import errno
 import random
 import subprocess
 import collections
+from copy import deepcopy
+
+
+Test = collections.namedtuple('Test', ['name', 'args', 'is_boost_test'])
+
 
 SERIAL_TESTS = [
-    'common/test/commonTest',
-    'tasking/test/taskingTest',
-    'kregistry/test/kregistryTest',
-    'hmemo/test/hmemoTest',
-    'blaskernel/test/blaskernelTest',
-    'utilskernel/test/utilskernelTest',
-    'sparsekernel/test/sparsekernelTest',
-    'dmemo/test/dmemoTest',
-    'lama/test/lamaTest',
-    'lama/test/storage/lamaStorageTest',
-    'lama/test/matrix/lamaMatrixTest',
-    'partitioning/test/partitioningTest',
-    'solver/test/solverTest'
+    Test('commonTest', [ 'common/test/commonTest' ], is_boost_test=True),
+    Test('loggingTest', [ 'logging/test/test.sh' ], is_boost_test=False),
+    Test('tracingTest', [ 'tracing/test/test.sh' ], is_boost_test=False),
+    Test('taskingTest', [ 'tasking/test/taskingTest' ], is_boost_test=True),
+    Test('kregistryTest', [ 'kregistry/test/kregistryTest' ], is_boost_test=True),
+    Test('hmemoTest', [ 'hmemo/test/hmemoTest' ], is_boost_test=True),
+    Test('blaskernelTest', [ 'blaskernel/test/blaskernelTest' ], is_boost_test=True),
+    Test('utilskernelTest', [ 'utilskernel/test/utilskernelTest' ], is_boost_test=True),
+    Test('sparsekernelTest', [ 'sparsekernel/test/sparsekernelTest' ], is_boost_test=True),
+    Test('dmemoTest', [ 'dmemo/test/dmemoTest' ], is_boost_test=True),
+    Test('lamaTest', [ 'lama/test/lamaTest' ], is_boost_test=True),
+    Test('lamaStorageTest', [ 'lama/test/storage/lamaStorageTest' ], is_boost_test=True),
+    Test('lamaMatrixTest', [ 'lama/test/matrix/lamaMatrixTest' ], is_boost_test=True),
+    Test('partitioningTest', [ 'partitioning/test/partitioningTest' ], is_boost_test=True),
+    Test('solverTest', [ 'solver/test/solverTest' ], is_boost_test=True)
 ]
 
 PASSED = " [ PASSED ] "
 FAILED = " [ FAILED ] "
-
-
-Test = collections.namedtuple('Test', ['name', 'path'])
 
 
 def ensure_directory_exists(dir_path):
@@ -40,22 +44,24 @@ def ensure_directory_exists(dir_path):
             raise
 
 
-def run_test(testname, path, output_dir):
+def run_test(test, output_dir):
     # Note: we are implicitly assuming unique test names here
-    report_path = os.path.join(output_dir, "{}_report.txt".format(testname))
-    log_path = os.path.join(output_dir, "{}_log.txt".format(testname))
-    stdout_path = os.path.join(output_dir, "{}_stdout.txt".format(testname))
-    stderr_path = os.path.join(output_dir, "{}_stderr.txt".format(testname))
+    report_path = os.path.join(output_dir, "{}_report.txt".format(test.name))
+    log_path = os.path.join(output_dir, "{}_log.txt".format(test.name))
+    stdout_path = os.path.join(output_dir, "{}_stdout.txt".format(test.name))
+    stderr_path = os.path.join(output_dir, "{}_stderr.txt".format(test.name))
 
-    args = [
-        path,
-        "--report_level=detailed",
-        "--report_format=HRF",
-        "--report_sink={}".format(report_path),
-        "--log_level=all",
-        "--log_format=HRF",
-        "--log_sink={}".format(log_path)
-    ]
+    args = deepcopy(test.args)
+
+    if test.is_boost_test:
+        args +=  [
+            "--report_level=detailed",
+            "--report_format=HRF",
+            "--report_sink={}".format(report_path),
+            "--log_level=all",
+            "--log_format=HRF",
+            "--log_sink={}".format(log_path)
+        ]
 
     ensure_directory_exists(output_dir)
 
@@ -64,23 +70,22 @@ def run_test(testname, path, output_dir):
         return retcode == 0
 
 
-def run_serial_tests(executable_paths, output_dir):
-    all_tests = [ Test(name=os.path.basename(path), path=path) for path in executable_paths ]
+def run_serial_tests(tests, output_dir):
     failed_tests = []
     successful_tests = []
     status_messages = [ "  Running test #{} {} ... ".format(index + 1, test.name)
-                       for (index, test) in enumerate(all_tests) ]
+                       for (index, test) in enumerate(tests) ]
     longest_message = max(status_messages, key=len)
 
-    print("Running {} serial tests ...".format(len(executable_paths)))
+    print("Running {} serial tests ...".format(len(tests)))
 
-    for (message, test) in zip(status_messages, all_tests):
+    for (message, test) in zip(status_messages, tests):
         padding = ' ' * ( len(longest_message) - len(message) )
         print(message + padding, end="")
         sys.stdout.flush()
 
         try:
-            passed = run_test(test.name, test.path, output_dir)
+            passed = run_test(test, output_dir)
         except Exception as e:
             passed = False;
             print("\nException when running test {}:\n{}\n".format(test.name, e))
@@ -96,7 +101,7 @@ def run_serial_tests(executable_paths, output_dir):
     result_label = PASSED if all_tests_passed else FAILED
     print("\nSerial tests completed. Results:")
     print("{}    {} tests run. {} successful tests. {} failed tests."
-            .format(result_label, len(executable_paths), len(successful_tests), len(failed_tests)))
+            .format(result_label, len(tests), len(successful_tests), len(failed_tests)))
 
     return len(failed_tests) == 0
 
