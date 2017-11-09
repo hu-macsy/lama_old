ea7d560ca93be4f78e36b15a1f5fa90ca89ffa52
Andreas Longva <andreas.borgen.longva@scai.fraunhofer.de>
2017-11-09 13:32:29 +0100

    Generate test HTML report in run_tests.py

--- scai/run_tests.py
+++ scai/run_tests.py
@@ -8,10 +8,16 @@ import errno
 import random
 import subprocess
 import collections
+import shutil
+import urlparse, urllib
+from glob import glob
 from copy import deepcopy
 
 
 Test = collections.namedtuple('Test', ['name', 'args', 'is_boost_test'])
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+TESTSUPPORT_DIR = os.path.join(SCRIPT_DIR, 'testsupport')
 
 
 SERIAL_TESTS = [
@@ -35,6 +41,34 @@ SERIAL_TESTS = [
 PASSED = " [ PASSED ] "
 FAILED = " [ FAILED ] "
 
+
+def url_for_file_path(file_path):
+    abs_path = os.path.abspath(file_path)
+    return urlparse.urljoin('file:', urllib.pathname2url(abs_path))
+
+
+def combine_reports(output_dir):
+    report_glob = os.path.join(output_dir, '*_report.xml')
+    report_path = os.path.join(output_dir, 'report.xml')
+    with open(report_path, 'w') as output_file:
+        output_file.write('<TestOutput>')
+        for report_file in glob(report_glob):
+            with open(report_file, 'r') as input_file:
+                shutil.copyfileobj(input_file, output_file)
+        output_file.write('</TestOutput>')
+
+    stylesheet_path = os.path.join(TESTSUPPORT_DIR, 'test_summary.xslt')
+    report_html_path = os.path.join(output_dir, 'report.html')
+
+    xsltproc_args = [
+        "xsltproc",
+        "-o",
+        report_html_path,
+        stylesheet_path,
+        report_path
+    ]
+    subprocess.check_call(xsltproc_args)
+    return report_html_path
 
 def ensure_directory_exists(dir_path):
     try:
@@ -98,6 +132,12 @@ def run_serial_tests(tests, output_dir):
     print("\nSerial tests completed. Results:")
     print("{}    {} tests run. {} successful tests. {} failed tests."
             .format(result_label, len(tests), len(successful_tests), len(failed_tests)))
+
+    print("Generating test report...")
+    html_report_path = combine_reports(output_dir)
+    print("Report successfully generated. Path to report:\n")
+    print(url_for_file_path(html_report_path))
+    print()
 
     return len(failed_tests) == 0
 
