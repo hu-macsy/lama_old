1ff199ff6846def4d43124f2757e8abaff38e997
Andreas Longva <andreas.borgen.longva@scai.fraunhofer.de>
2017-11-22 13:13:41 +0100

    Document uniquePath functions

--- scai/testsupport/include/scai/testsupport/uniquePath.hpp
+++ scai/testsupport/include/scai/testsupport/uniquePath.hpp
@@ -11,6 +11,18 @@ namespace scai
 namespace testsupport
 {
 
+/**
+ * Generate a path extremely likely to be unique.
+ *
+ * Generates a path in the given directory path which is extremely likely to be unique.
+ * The pattern of the generated path is given by:
+ *
+ * dir/namePrefixRANDOM
+ *
+ * where RANDOM is a random sequence of alpha-numeric characters of unspecified length.
+ *
+ * Note: The path manipulation is extremely rudimentary and entirely string-based.
+ */
 std::string uniquePath(const std::string & dir, const std::string & namePrefix = "");
 
 } // namespace testsupport
--- scai/testsupport/include/scai/testsupport/uniquePathComm.hpp
+++ scai/testsupport/include/scai/testsupport/uniquePathComm.hpp
@@ -12,25 +12,35 @@ namespace scai
 namespace testsupport
 {
 
-    std::string uniquePathPerNode(const std::string & unique_dir,
+    /**
+     * Generate a unique path which is unique also among the nodes in the communicator.
+     *
+     * See uniquePath() for the role of `dir` and `namePrefix`.
+     */
+    std::string uniquePathPerNode(const std::string & dir,
                                   const scai::dmemo::Communicator & comm,
                                   const std::string & namePrefix = "")
     {
         std::stringstream path;
-        path << uniquePath(unique_dir, namePrefix);
+        path << uniquePath(dir, namePrefix);
         path << '_' << comm.getSize() << '_' << comm.getRank();
         return path.str();
     }
 
-    std::string uniquePathSharedAmongNodes(const std::string & unique_dir,
+    /**
+     * Generate a unique path which is shared among the nodes in the communicator.
+     *
+     * See uniquePath() for the role of `dir` and `namePrefix`.
+     */
+    std::string uniquePathSharedAmongNodes(const std::string & dir,
                                            const scai::dmemo::Communicator & comm,
                                            const std::string & namePrefix = "")
     {
-        // TODO: Make exception-safe
+        // TODO: Make exception-safe with respect to MPI
         std::string path;
         if (comm.getRank() == 0)
         {
-            path = ::scai::testsupport::uniquePath(unique_dir, namePrefix);
+            path = ::scai::testsupport::uniquePath(dir, namePrefix);
         }
         comm.bcast(path, 0);
         return path;
