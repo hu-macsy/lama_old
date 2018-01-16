af38611c0ca0d88d3b3fde02e0654ccac9cee394
Andreas Longva <andreas.borgen.longva@scai.fraunhofer.de>
2018-01-16 10:41:41 +0100

    Provide only writeOnlyAccess with newSize param

--- scai/hmemo/WriteOnlyAccess.hpp
+++ scai/hmemo/WriteOnlyAccess.hpp
@@ -140,9 +140,9 @@ private:
  * motivation and intended usage of this function.
  */
 template <typename ValueType>
-WriteOnlyAccess<ValueType> writeOnlyAccess(HArray<ValueType> & array)
+WriteOnlyAccess<ValueType> writeOnlyAccess(HArray<ValueType> & array, IndexType newSize)
 {
-    return WriteOnlyAccess<ValueType>(array);
+    return WriteOnlyAccess<ValueType>(array, newSize);
 }
 
 /**
@@ -152,9 +152,9 @@ WriteOnlyAccess<ValueType> writeOnlyAccess(HArray<ValueType> & array)
  * motivation and intended usage of this function.
  */
 template <typename ValueType>
-WriteOnlyAccess<ValueType> writeOnlyAccess(HArray<ValueType> & array, ContextPtr context)
+WriteOnlyAccess<ValueType> writeOnlyAccess(HArray<ValueType> & array, ContextPtr context, IndexType newSize)
 {
-    return WriteOnlyAccess<ValueType>(array, context);
+    return WriteOnlyAccess<ValueType>(array, context, newSize);
 }
 
 } /* end namespace hmemo */
