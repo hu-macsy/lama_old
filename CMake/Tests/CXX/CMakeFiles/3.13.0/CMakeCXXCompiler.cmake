e6065d78223634a24c6b2ac729bb6bcc42bb56db
Thomas Brandes <brandes1@juwels08.fz-juelich.de>
2019-04-04 10:03:57 +0200

    Added latest CUDA compute capabilities

---
+++ CMake/Tests/CXX/CMakeFiles/3.13.0/CMakeCXXCompiler.cmake
@@ -0,0 +1,76 @@
+set(CMAKE_CXX_COMPILER "/gpfs/software/juwels/stages/2018b/software/icc/2019.0.117-GCC-7.3.0/compilers_and_libraries_2019.0.117/linux/bin/intel64/icpc")
+set(CMAKE_CXX_COMPILER_ARG1 "")
+set(CMAKE_CXX_COMPILER_ID "Intel")
+set(CMAKE_CXX_COMPILER_VERSION "19.0.0.20180804")
+set(CMAKE_CXX_COMPILER_VERSION_INTERNAL "")
+set(CMAKE_CXX_COMPILER_WRAPPER "")
+set(CMAKE_CXX_STANDARD_COMPUTED_DEFAULT "14")
+set(CMAKE_CXX_COMPILE_FEATURES "cxx_std_98;cxx_binary_literals;cxx_std_11;cxx_alias_templates;cxx_alignas;cxx_alignof;cxx_attributes;cxx_auto_type;cxx_constexpr;cxx_decltype;cxx_decltype_incomplete_return_types;cxx_default_function_template_args;cxx_defaulted_functions;cxx_defaulted_move_initializers;cxx_delegating_constructors;cxx_deleted_functions;cxx_enum_forward_declarations;cxx_explicit_conversions;cxx_extended_friend_declarations;cxx_extern_templates;cxx_final;cxx_func_identifier;cxx_generalized_initializers;cxx_inheriting_constructors;cxx_inline_namespaces;cxx_lambdas;cxx_local_type_template_args;cxx_long_long_type;cxx_noexcept;cxx_nonstatic_member_init;cxx_nullptr;cxx_override;cxx_range_for;cxx_raw_string_literals;cxx_reference_qualified_functions;cxx_right_angle_brackets;cxx_rvalue_references;cxx_sizeof_member;cxx_static_assert;cxx_strong_enums;cxx_template_template_parameters;cxx_thread_local;cxx_trailing_return_types;cxx_unicode_literals;cxx_uniform_initialization;cxx_unrestricted_unions;cxx_user_literals;cxx_variadic_macros;cxx_variadic_templates;cxx_std_14;cxx_aggregate_default_initializers;cxx_attribute_deprecated;cxx_contextual_conversions;cxx_decltype_auto;cxx_digit_separators;cxx_generic_lambdas;cxx_lambda_init_captures;cxx_relaxed_constexpr;cxx_return_type_deduction;cxx_variable_templates;cxx_std_17")
+set(CMAKE_CXX98_COMPILE_FEATURES "cxx_std_98;cxx_binary_literals")
+set(CMAKE_CXX11_COMPILE_FEATURES "cxx_std_11;cxx_alias_templates;cxx_alignas;cxx_alignof;cxx_attributes;cxx_auto_type;cxx_constexpr;cxx_decltype;cxx_decltype_incomplete_return_types;cxx_default_function_template_args;cxx_defaulted_functions;cxx_defaulted_move_initializers;cxx_delegating_constructors;cxx_deleted_functions;cxx_enum_forward_declarations;cxx_explicit_conversions;cxx_extended_friend_declarations;cxx_extern_templates;cxx_final;cxx_func_identifier;cxx_generalized_initializers;cxx_inheriting_constructors;cxx_inline_namespaces;cxx_lambdas;cxx_local_type_template_args;cxx_long_long_type;cxx_noexcept;cxx_nonstatic_member_init;cxx_nullptr;cxx_override;cxx_range_for;cxx_raw_string_literals;cxx_reference_qualified_functions;cxx_right_angle_brackets;cxx_rvalue_references;cxx_sizeof_member;cxx_static_assert;cxx_strong_enums;cxx_template_template_parameters;cxx_thread_local;cxx_trailing_return_types;cxx_unicode_literals;cxx_uniform_initialization;cxx_unrestricted_unions;cxx_user_literals;cxx_variadic_macros;cxx_variadic_templates")
+set(CMAKE_CXX14_COMPILE_FEATURES "cxx_std_14;cxx_aggregate_default_initializers;cxx_attribute_deprecated;cxx_contextual_conversions;cxx_decltype_auto;cxx_digit_separators;cxx_generic_lambdas;cxx_lambda_init_captures;cxx_relaxed_constexpr;cxx_return_type_deduction;cxx_variable_templates")
+set(CMAKE_CXX17_COMPILE_FEATURES "cxx_std_17")
+set(CMAKE_CXX20_COMPILE_FEATURES "")
+
+set(CMAKE_CXX_PLATFORM_ID "Linux")
+set(CMAKE_CXX_SIMULATE_ID "")
+set(CMAKE_CXX_SIMULATE_VERSION "")
+
+
+
+set(CMAKE_AR "/gpfs/software/juwels/stages/2018b/software/binutils/2.31.1-GCCcore-7.3.0/bin/ar")
+set(CMAKE_CXX_COMPILER_AR "")
+set(CMAKE_RANLIB "/gpfs/software/juwels/stages/2018b/software/binutils/2.31.1-GCCcore-7.3.0/bin/ranlib")
+set(CMAKE_CXX_COMPILER_RANLIB "")
+set(CMAKE_LINKER "/gpfs/software/juwels/stages/2018b/software/binutils/2.31.1-GCCcore-7.3.0/bin/ld")
+set(CMAKE_COMPILER_IS_GNUCXX )
+set(CMAKE_CXX_COMPILER_LOADED 1)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_ABI_COMPILED TRUE)
+set(CMAKE_COMPILER_IS_MINGW )
+set(CMAKE_COMPILER_IS_CYGWIN )
+if(CMAKE_COMPILER_IS_CYGWIN)
+  set(CYGWIN 1)
+  set(UNIX 1)
+endif()
+
+set(CMAKE_CXX_COMPILER_ENV_VAR "CXX")
+
+if(CMAKE_COMPILER_IS_MINGW)
+  set(MINGW 1)
+endif()
+set(CMAKE_CXX_COMPILER_ID_RUN 1)
+set(CMAKE_CXX_IGNORE_EXTENSIONS inl;h;hpp;HPP;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_CXX_SOURCE_FILE_EXTENSIONS C;M;c++;cc;cpp;cxx;mm;CPP)
+set(CMAKE_CXX_LINKER_PREFERENCE 30)
+set(CMAKE_CXX_LINKER_PREFERENCE_PROPAGATES 1)
+
+# Save compiler ABI information.
+set(CMAKE_CXX_SIZEOF_DATA_PTR "8")
+set(CMAKE_CXX_COMPILER_ABI "ELF")
+set(CMAKE_CXX_LIBRARY_ARCHITECTURE "")
+
+if(CMAKE_CXX_SIZEOF_DATA_PTR)
+  set(CMAKE_SIZEOF_VOID_P "${CMAKE_CXX_SIZEOF_DATA_PTR}")
+endif()
+
+if(CMAKE_CXX_COMPILER_ABI)
+  set(CMAKE_INTERNAL_PLATFORM_ABI "${CMAKE_CXX_COMPILER_ABI}")
+endif()
+
+if(CMAKE_CXX_LIBRARY_ARCHITECTURE)
+  set(CMAKE_LIBRARY_ARCHITECTURE "")
+endif()
+
+set(CMAKE_CXX_CL_SHOWINCLUDES_PREFIX "")
+if(CMAKE_CXX_CL_SHOWINCLUDES_PREFIX)
+  set(CMAKE_CL_SHOWINCLUDES_PREFIX "${CMAKE_CXX_CL_SHOWINCLUDES_PREFIX}")
+endif()
+
+
+
+
+
+set(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "imf;svml;irng;stdc++;m;ipgo;decimal;cilkrts;stdc++;gcc;gcc_s;irc;svml;c;gcc;gcc_s;irc_s;dl;c")
+set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "/gpfs/software/juwels/stages/2018b/software/Boost/1.68.0-iimpi-2018b.1-Python-2.7.15/lib;/gpfs/software/juwels/stages/2018b/software/ICU/61.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/Python/2.7.15-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/libjpeg-turbo/1.5.3-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/libspatialindex/1.8.5-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/gflags/2.2.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/protobuf/3.6.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/PostgreSQL/10.3-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/Java/1.8/lib;/gpfs/software/juwels/stages/2018b/software/libyaml/0.2.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/libffi/3.2.1-GCCcore-7.3.0/lib64;/gpfs/software/juwels/stages/2018b/software/libffi/3.2.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/libxslt/1.1.32-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/libxml2/2.9.7-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/XZ/5.2.4-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/GMP/6.1.2-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/Tk/8.6.8-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/X11/20180604-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/fontconfig/2.13.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/util-linux/2.32.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/freetype/2.9.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/libpng/1.6.35-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/expat/2.2.6-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/SQLite/3.22.0-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/Tcl/8.6.8-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/libreadline/7.0-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/zlib/1.2.11-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/bzip2/1.0.6-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/CUDA/9.2.88/lib64/stubs;/gpfs/software/juwels/stages/2018b/software/CUDA/9.2.88/lib64;/gpfs/software/juwels/stages/2018b/software/impi/2018.4.274-iccifort-2019.0.117-GCC-7.3.0/lib64;/gpfs/software/juwels/stages/2018b/software/ifort/2019.0.117-GCC-7.3.0/compilers_and_libraries_2019.0.117/linux/mpi/intel64;/gpfs/software/juwels/stages/2018b/software/ifort/2019.0.117-GCC-7.3.0/compilers_and_libraries_2019.0.117/linux/compiler/lib/intel64;/gpfs/software/juwels/stages/2018b/software/icc/2019.0.117-GCC-7.3.0/compilers_and_libraries_2019.0.117/linux/tbb/lib/intel64/gcc4.4;/gpfs/software/juwels/stages/2018b/software/icc/2019.0.117-GCC-7.3.0/compilers_and_libraries_2019.0.117/linux/compiler/lib/intel64;/gpfs/software/juwels/stages/2018b/software/ncurses/6.1-GCCcore-7.3.0/lib;/gpfs/software/global/jscslurm/opt/slurm-jsc1711/lib;/gpfs/software/juwels/stages/2018b/software/binutils/2.31.1-GCCcore-7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/GCCcore/7.3.0/lib64;/gpfs/software/juwels/stages/2018b/software/GCCcore/7.3.0/lib;/gpfs/software/juwels/stages/2018b/software/icc/2019.0.117-GCC-7.3.0/compilers_and_libraries_2019.0.117/linux/compiler/lib/intel64_lin;/gpfs/software/juwels/stages/2018b/software/GCCcore/7.3.0/lib/gcc/x86_64-pc-linux-gnu/7.3.0;/lib64;/usr/lib64;/lib;/usr/lib")
+set(CMAKE_CXX_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
