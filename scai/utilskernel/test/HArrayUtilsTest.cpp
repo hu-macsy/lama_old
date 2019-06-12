004671510e839e04e2d3a0e81e91febe8b3cf08d
Thomas.Brandes@scai.fraunhofer.de <Thomas.Brandes@scai.fraunhofer.de>
2019-06-12 11:44:32 +0200

    Disabled all tests for comparison with complex numbers.

--- scai/utilskernel/test/HArrayUtilsTest.cpp
+++ scai/utilskernel/test/HArrayUtilsTest.cpp
@@ -734,10 +734,13 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( allTest, ValueType, scai_numeric_test_types )
     BOOST_CHECK( HArrayUtils::all( dummy0, common::CompareOp::EQ, dummy1, ctx ) );
     BOOST_CHECK( HArrayUtils::all( dummy0, common::CompareOp::NE, dummy1, ctx ) );
 
-    BOOST_CHECK( HArrayUtils::all( array1, common::CompareOp::LT, array2, ctx ) );
-    BOOST_CHECK( !HArrayUtils::all( array1, common::CompareOp::GE, array2, ctx ) );
-    BOOST_CHECK( !HArrayUtils::all( array1, common::CompareOp::LT, array3, ctx ) );
-    BOOST_CHECK( HArrayUtils::all( array1, common::CompareOp::LE, array3, ctx ) );
+    if ( !common::isComplex( common::TypeTraits<ValueType>::stype ) )
+    {
+        BOOST_CHECK( HArrayUtils::all( array1, common::CompareOp::LT, array2, ctx ) );
+        BOOST_CHECK( !HArrayUtils::all( array1, common::CompareOp::GE, array2, ctx ) );
+        BOOST_CHECK( !HArrayUtils::all( array1, common::CompareOp::LT, array3, ctx ) );
+        BOOST_CHECK( HArrayUtils::all( array1, common::CompareOp::LE, array3, ctx ) );
+    }
 }
 
 /* --------------------------------------------------------------------- */
